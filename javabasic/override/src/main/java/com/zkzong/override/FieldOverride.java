a977907372389f268663abdb77d55fec376da519
zongzhankui <zongzhankui@gomefinance.com.cn>
2019-08-20 19:33:04 +0800

    override

--- javabasic/override/src/main/java/com/zkzong/override/FieldOverride.java
+++ javabasic/override/src/main/java/com/zkzong/override/FieldOverride.java
@@ -14,6 +14,13 @@ public class FieldOverride {
         System.out.println(c1.s);
         Super c2 = new Sub();
         System.out.println(c2.s);
+        /**
+         * Sub
+         * Super
+         */
+        // 在类中，与父类中的字段具有相同名称的字段为隐藏字段，即使它们的类型不同。
+        // 该字段必须通过父类访问。
+        // 成员字段不能像方法一样被重写。
     }
 }
 
