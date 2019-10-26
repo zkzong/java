9c11aa3aee175654f1ddd0f70cfa8e0e20050488
zkzong <zongzhankui@hotmail.com>
2019-10-26 19:09:17 +0800

    concurrent

---
+++ javabasic/thread/concurrent/src/main/java/com/zkzong/concurrent/QueueTest.java
@@ -0,0 +1,55 @@
+package com.zkzong.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * @Author: zkzong
+ * @Date: 2019/10/26
+ */
+public class QueueTest {
+
+    // ArrayBlockingQueue
+    @Test
+    public void t1() {
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(5);
+        queue.add("1");
+        // 插入null报错
+        queue.add(null);
+        System.out.println(queue);
+    }
+
+    // LinkedBlockingQueue
+    @Test
+    public void t2() {
+        BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+        queue.add("1");
+        // 插入null报错
+        queue.add(null);
+        System.out.println(queue);
+    }
+
+    // PriorityBlockingQueue
+    @Test
+    public void t3() {
+        BlockingQueue<String> queue = new PriorityBlockingQueue<>(5);
+        queue.add("1");
+        // 插入null报错
+        queue.add(null);
+        System.out.println(queue);
+    }
+
+    // SynchronousQueue
+    @Test
+    public void t4() {
+        BlockingQueue<String> queue = new SynchronousQueue<String>();
+        // 只能插入一个元素
+        queue.add("1");
+        System.out.println(queue);
+    }
+}
