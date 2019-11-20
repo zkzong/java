1fda87ebf9fc83bd2623eedeaacf4b0e8cc5dcd6
zongzhankui <zongzhankui@gomefinance.com.cn>
2019-11-20 20:36:07 +0800

    pdf

---
+++ thirdparty/pdf/itext/src/main/java/com/zkzong/itext/sign/KeywordPDFUtils.java
@@ -0,0 +1,163 @@
+package com.zkzong.itext.sign;
+
+import com.google.common.collect.Lists;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.ImageRenderInfo;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.RenderListener;
+import com.itextpdf.text.pdf.parser.TextRenderInfo;
+import com.zkzong.itext.sign.bean.KeyWordBean;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KeywordPDFUtils {
+    // 定义返回页码
+    private static int i = 0;
+    private static com.itextpdf.awt.geom.Rectangle2D.Float boundingRectange = null;
+
+    private static List<KeyWordBean> lists = null;
+
+    // private static KeyWordBean bean = new KeyWordBean();
+
+    private static Map<Integer, List<KeyWordBean>> map = new HashMap<Integer, List<KeyWordBean>>();
+
+    public static Map<Integer, List<KeyWordBean>> getPDFText(String filePath) {
+        try {
+            PdfReader pdfReader = new PdfReader(filePath);
+            int pageNum = pdfReader.getNumberOfPages();
+            PdfReaderContentParser pdfReaderContentParser = new PdfReaderContentParser(pdfReader);
+
+            for (i = 1; i <= pageNum; i++) {
+                lists = Lists.newArrayList();
+                boundingRectange = new com.itextpdf.awt.geom.Rectangle2D.Float();
+                pdfReaderContentParser.processContent(i, new RenderListener() {
+                    @Override
+                    public void renderText(TextRenderInfo textRenderInfo) {
+                        String text = textRenderInfo.getText(); // 整页内容
+                        boundingRectange = textRenderInfo.getBaseline().getBoundingRectange();
+                        KeyWordBean bean = new KeyWordBean();
+                        bean.setX(boundingRectange.x);
+                        bean.setY(boundingRectange.y);
+                        bean.setPage(i);
+                        bean.setText(text);
+                        lists.add(bean);
+                    }
+
+                    @Override
+                    public void renderImage(ImageRenderInfo arg0) {
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void endTextBlock() {
+                        // TODO Auto-generated method stub
+                    }
+
+                    @Override
+                    public void beginTextBlock() {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+                map.put(i, lists);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, List<KeyWordBean>> keyWords = getPDFText("D:\\pdf\\src.pdf");
+        System.out.println(keyWords.size());
+    }
+
+    public static KeyWordBean getKeyWordXY(Map<Integer, List<KeyWordBean>> map2, int page, int num, String keyWord) {
+        int keyMatch = 1;
+        StringBuilder content = new StringBuilder();
+        List<KeyWordBean> list = map.get(page);
+        Collections.sort(list);//正序比较
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            KeyWordBean bean = list.get(i);
+            String text = bean.getText();
+            if (i + 1 != list.size()) {
+                KeyWordBean beanNext = list.get(i + 1);
+                float x = beanNext.getX() - bean.getX();
+                float y = beanNext.getY() - bean.getY();
+                if (y == 0 && x <= 1) {// System.out.print(“去除因字体加粗产生的重复数字”);
+                } else {
+                    if (StringUtils.contains(content.toString(), keyWord) || StringUtils.contains(text, keyWord)) {
+                        if (keyMatch == num) {
+                            return bean;
+                        } else {
+                            keyMatch++;
+                        }
+                    } else if ((!StringUtils.isEmpty(text) && keyWord.startsWith(text)) || content.length() > 0) {
+                        content.append(text);
+                        if (content.length() >= keyWord.length()) {
+                            if (StringUtils.contains(content.toString(), keyWord)) {
+                                if (keyMatch == num) {
+                                    return bean;
+                                } else {
+                                    keyMatch++;
+                                }
+                            }
+                            content = new StringBuilder();
+                        }
+                    }
+                }
+            } else {
+                if (StringUtils.contains(content.toString(), keyWord) || StringUtils.contains(text, keyWord)) {
+                    if (keyMatch == num) {
+                        return bean;
+                    } else {
+                        keyMatch++;
+                    }
+                } else if ((!StringUtils.isEmpty(text) && keyWord.startsWith(text)) || content.length() > 0) {
+                    content.append(text);
+                    if (content.length() >= keyWord.length()) {
+                        if (StringUtils.contains(content.toString(), keyWord)) {
+                            if (keyMatch == num) {
+                                return bean;
+                            } else {
+                                keyMatch++;
+                            }
+                        }
+                        content = new StringBuilder();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static KeyWordBean getKeyWordBean(KeyWordBean bean, StringBuilder content, String keyWord, String text, int keyMatch, int num) {
+        if (StringUtils.contains(content.toString(), keyWord) || StringUtils.contains(text, keyWord)) {
+            if (keyMatch == num) {
+                return bean;
+            } else {
+                keyMatch++;
+            }
+        } else if ((!StringUtils.isEmpty(text) && keyWord.startsWith(text)) || content.length() > 0) {
+            content.append(text);
+            if (content.length() >= keyWord.length()) {
+                if (StringUtils.contains(content.toString(), keyWord)) {
+                    if (keyMatch == num) {
+                        return bean;
+                    } else {
+                        keyMatch++;
+                    }
+                }
+                content = new StringBuilder();
+            }
+        }
+        return null;
+
+    }
+}
---
+++ thirdparty/pdf/itext/src/main/java/com/zkzong/itext/sign/Test.java
@@ -0,0 +1,106 @@
+package com.zkzong.itext.sign;
+
+import com.zkzong.itext.sign.bean.KeyWordBean;
+import com.zkzong.itext.sign.bean.SignPDFBean;
+import com.zkzong.itext.sign.bean.SignPDFRequestBean;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        /*
+         * 签章参数
+         */
+        // String keyWord = "85000315"; // 签章关键字
+        // int page = 1; // 关键字所在页数
+        // int num = 1; // 取第n次出现的关键字
+        // String srcPDFPath = "D:\pdf\test2.pdf"; // 待签章pdf文件路径
+        // String outPDFPath = "D:\pdf\out.pdf"; // 签章后输出的pdf文件路径
+        // String sealPath = "D:\pdf\印章.png"; // 印章图片路径
+        // String keyStorePath = "D:\pdf\keystore"; // 证书文件路径
+        // String keyStorePass = "Jitadmin001"; // 证书密码
+        // String signReason = "设置签章原因"; // 设置签章原因，可以为空
+        // String signLocation = "设置签章地点 "; // 设置签章地点
+        List list = new ArrayList();
+        SignPDFBean bean1 = new SignPDFBean();
+        bean1.setKeyStorePass("11111111");
+        bean1.setKeyStorePath("D:/pdf/key/keystore");
+        // bean1.setKeyStorePass("Jitadmin001");
+        // bean1.setKeyStorePath("D:\pdf\keystore");
+        bean1.setKeyWord("85000315");
+        bean1.setNum(1);
+        bean1.setPage(1);
+        bean1.setSealPath("D:/pdf/印章.png");
+        bean1.setSignLocation("知春路1");
+        bean1.setSignReason("孙测试1");
+        SignPDFBean bean2 = new SignPDFBean();
+        bean2.setKeyStorePass("Jitadmin001");
+        bean2.setKeyStorePath("D:\\pdf\\keystore");
+        bean2.setKeyWord("客户");
+        bean2.setNum(1);
+        bean2.setPage(1);
+        bean2.setSealPath("D:\\pdf\\印章2.png");
+        bean2.setSignLocation("知春路2");
+        bean2.setSignReason("孙测试2");
+
+        SignPDFBean bean3 = new SignPDFBean();
+        bean3.setKeyStorePass("Jitadmin001");
+        bean3.setKeyStorePath("D:\\pdf\\keystore");
+        bean3.setKeyWord("五");
+        bean3.setNum(1);
+        bean3.setPage(1);
+        bean3.setSealPath("D:\\pdf\\印章3.png");
+        bean3.setSignLocation("知春路3");
+        bean3.setSignReason("孙测试3");
+
+        list.add(bean1);
+        list.add(bean2);
+        list.add(bean3);
+
+        SignPDFRequestBean requestBean = new SignPDFRequestBean();
+        requestBean.setSrcPDFPath("D:\\pdf\\test2.pdf");
+        requestBean.setOutPDFPath("D:\\pdf\\out.pdf");
+        requestBean.setSignPDFBeans(list);
+
+        long startTime = System.currentTimeMillis();
+        // 1.解析pdf文件
+        Map<Integer, List<KeyWordBean>> map = KeywordPDFUtils.getPDFText(requestBean.getSrcPDFPath());
+        // 2.获取关键字坐标
+        List<SignPDFBean> beans = requestBean.getSignPDFBeans();
+        byte[] fileData = null;
+        InputStream in = null;
+        for (int i = 0; i < beans.size(); i++) {
+            SignPDFBean pdfBean = beans.get(i);
+            KeyWordBean bean = KeywordPDFUtils.getKeyWordXY(map, pdfBean.getPage(), pdfBean.getNum(), pdfBean.getKeyWord());
+            if (null == bean) {
+                System.out.println("未查询到关键字。。。");
+            }
+            System.out.println(bean.toString());
+            long keyTime = System.currentTimeMillis();
+            if (i == 0) {
+                in = new FileInputStream(requestBean.getSrcPDFPath());
+            } else {
+                in = new ByteArrayInputStream(fileData);
+            }
+            // 3.进行盖章
+            fileData = SignPDFUtils.sign(pdfBean.getKeyStorePass(), pdfBean.getKeyStorePath(), in, pdfBean.getSealPath(), bean.getX(), bean.getY(), bean.getPage(), pdfBean.getSignReason(), pdfBean.getSignLocation());
+            long signTime = System.currentTimeMillis();
+        }
+        // 4.输出盖章后pdf文件
+        FileOutputStream f = new FileOutputStream(new File(requestBean.getOutPDFPath()));
+        f.write(fileData);
+        f.close();
+        in.close();
+        long endTime = System.currentTimeMillis();
+        // System.out.println("关键字定位花费时间："+(keyTime-startTime));
+        // System.out.println("盖章消耗时间："+(signTime-keyTime));
+        System.out.println("总时间：" + (endTime - startTime));
+    }
+}
