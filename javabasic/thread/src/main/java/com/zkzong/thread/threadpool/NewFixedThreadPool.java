2508cd6a493d2022b0b9ae393e51cb9c105de722
zkzong <zongzhankui@hotmail.com>
2019-11-22 23:24:43 +0800

    Executors

--- javabasic/thread/src/main/java/com/zkzong/thread/threadpool/NewFixedThreadPool.java
+++ javabasic/thread/src/main/java/com/zkzong/thread/threadpool/NewFixedThreadPool.java
@@ -12,16 +12,33 @@ public class NewFixedThreadPool {
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
         for (int i = 0; i < 10; i++) {
             final int index = i;
-            fixedThreadPool.execute(new Runnable() {
+            fixedThreadPool.submit(new Runnable() {
                 @Override
                 public void run() {
+                    //try {
+                    //    System.out.println(index);
+                    //    Thread.sleep(2000);
+                    //} catch (InterruptedException e) {
+                    //    e.printStackTrace();
+                    //}
+                    System.out.println("===");
+                    Object obj = null;
                     try {
-                        System.out.println(index);
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
+                        System.out.println(obj.toString());
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
+            });
+
+
+            ExecutorService threadPool = Executors.newFixedThreadPool(1, r -> {
+                Thread t = new Thread(r);
+                t.setUncaughtExceptionHandler(
+                        (t1, e) -> {
+                            System.out.println(t1.getName() + "线程抛出的异常" + e);
+                        });
+                return t;
             });
         }
     }
