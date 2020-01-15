728fe8fc09ce20c2e8fa28f4fb7367154c339668
zongzhankui <zongzhankui@syxfjr.cn>
2020-01-15 19:39:53 +0800

    enum

---
+++ javabasic/enum/src/main/java/com/zkzong/guava/ResultEnum.java
@@ -0,0 +1,34 @@
+package com.zkzong.guava;
+
+/**
+ * @Author: zong
+ * @Date: 2020/1/9
+ */
+public enum ResultEnum {
+    SUCCESS("0000", "成功"),
+    FAIL("9999", "失败");
+
+    private String code;
+    private String message;
+
+    ResultEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
