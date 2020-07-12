ab9ee3cc1eefd273ed722f07beb7f13bd5f4c967
zongzhankui <zongzhankui@syxfjr.cn>
2020-07-12 21:27:49 +0800

    java

---
+++ thirdparty/fastdfs/fastdfs-spring-oschina/src/test/java/com/zkzong/fastdfs/client/FastDFSClientTest.java
@@ -0,0 +1,34 @@
+package com.zkzong.fastdfs.client;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Author: zkzong
+ * @Date: 2018.10.17
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:/spring/applicationContext.xml"})
+public class FastDFSClientTest {
+
+    private FastDFSClient fastDFSClient;
+
+    @Before
+    public void before() {
+        fastDFSClient = new FastDFSClient();
+    }
+
+    @Test
+    public void uploadFileWithFilepath() {
+        String filePath = "../file/googlelogo.png";
+        try {
+            String path = fastDFSClient.uploadFileWithFilepath(filePath);
+            System.out.println(path);
+        } catch (FastDFSException e) {
+            e.printStackTrace();
+        }
+    }
+}
