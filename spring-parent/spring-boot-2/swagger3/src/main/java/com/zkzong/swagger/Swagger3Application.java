22ee55cde01f9097427ae8e6d2ef17a74118d7b1
zongzhankui <zongzhankui@hotmail.com>
2020-11-30 20:13:50 +0800

    swagger3

---
+++ spring-parent/spring-boot-2/swagger3/src/main/java/com/zkzong/swagger/Swagger3Application.java
@@ -0,0 +1,11 @@
+package com.zkzong.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger3Application.class, args);
+    }
+}
