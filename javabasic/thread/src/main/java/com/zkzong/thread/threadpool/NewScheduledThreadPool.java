580c04e7363f74a301a760bc80f61ae4188d95bc
zongzhankui <zongzhankui@hotmail.com>
2020-12-06 21:57:39 +0800

    thread pool

--- javabasic/thread/src/main/java/com/zkzong/thread/threadpool/NewScheduledThreadPool.java
+++ javabasic/thread/src/main/java/com/zkzong/thread/threadpool/NewScheduledThreadPool.java
@@ -1,5 +1,6 @@
 package com.zkzong.thread.threadpool;
 
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -11,17 +12,18 @@ import java.util.concurrent.TimeUnit;
 public class NewScheduledThreadPool {
     public static void main(String[] args) {
         ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        System.out.println(new Date());
         scheduledThreadPool.schedule(new Runnable() {
             @Override
             public void run() {
-                System.out.println("delay 3 seconds");
+                System.out.println(new Date() + " delay 3 seconds");
             }
         }, 3, TimeUnit.SECONDS);
 
         scheduledThreadPool.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-                System.out.println("delay 1 seconds, and execute every 3 seconds");
+                System.out.println(new Date() + " delay 1 seconds, and execute every 3 seconds");
             }
         }, 1, 3, TimeUnit.SECONDS);
     }
