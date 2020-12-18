a6417b6263a09498fc509378d5b2de2b84598ebc
zongzhankui <zongzhankui@hotmail.com>
2020-12-18 17:51:12 +0800

    elastic-job

--- job-scheduler/elastic-job/elastic-job-demo/elastic-job-lite-springboot/src/main/resources/db/data.sql
+++
@@ -1,3 +0,0 @@
-insert into foo(location, status) values ('Beijing', 'TODO');
-insert into foo(location, status) values ('Shanghai', 'TODO');
-insert into foo(location, status) values ('Guangzhou', 'TODO');
--- job-scheduler/elastic-job/elastic-job-demo/elastic-job-lite-springboot/src/main/resources/db/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE foo(
-	id int(10) NOT NULL auto_increment PRIMARY KEY,
-	location varchar(20),
-	status varchar(20)
-);
