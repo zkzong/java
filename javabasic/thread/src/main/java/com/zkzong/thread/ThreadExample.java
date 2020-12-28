1349c51fdcc4c68f15df6ccd37c09e0bc3548ef8
zongzhankui <zongzhankui@hotmail.com>
2020-12-28 17:40:16 +0800

    thread

--- javabasic/thread/src/main/java/com/zkzong/thread/ThreadExample.java
+++ javabasic/thread/src/main/java/com/zkzong/thread/ThreadExample.java
@@ -1,10 +1,13 @@
 package com.zkzong.thread;
 
+import org.junit.Test;
+
 /**
  * Created by Zong on 2016/7/28.
  */
 public class ThreadExample {
-    public static void main(String[] args) {
+    @Test
+    public void main() {
         System.out.println(Thread.currentThread().getName());
         for (int i = 0; i < 10; i++) {
             new Thread("" + i) {
@@ -14,5 +17,40 @@ public class ThreadExample {
                 }
             }.start();
         }
+    }
+
+    /**
+     * 只是new Exception对象不会抛出异常
+     */
+    @Test
+    public void nothrow() {
+        for (int i = 0; i < 500; i++) {
+            final int j = i;
+            //System.out.println(j);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    new RuntimeException(j + "");
+                    System.out.println(j);
+                }
+            }).start();
+        }
+    }
+
+    /**
+     * throw Exception
+     */
+    @Test
+    public void throwexception() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    throw new Exception();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
