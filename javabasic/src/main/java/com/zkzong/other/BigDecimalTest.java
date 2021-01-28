7550bd4906d7659b3b9f5b890eac7f06bb5b9f79
zongzhankui <zongzhankui@hotmail.com>
2021-01-28 15:13:13 +0800

    BigDecimal

--- javabasic/src/main/java/com/zkzong/other/BigDecimalTest.java
+++ javabasic/src/main/java/com/zkzong/other/BigDecimalTest.java
@@ -9,11 +9,31 @@ import java.math.BigDecimal;
  */
 public class BigDecimalTest {
     public static void main(String[] args) {
-        BigDecimal bg = new BigDecimal("0.00");
+        // 1. 使用BigDecimal(String val)的构造方法创建对象
+        System.out.println(new BigDecimal("1.745"));
+        System.out.println(new BigDecimal("0.745"));
+        // 2. 使用使用BigDecimal的valueOf(double val)方法创建对象
+        System.out.println(BigDecimal.valueOf(1.745));
+        System.out.println(BigDecimal.valueOf(0.745));
 
         BigDecimal a = new BigDecimal(0.00);
+        BigDecimal b = new BigDecimal("0.00");
         BigDecimal c = new BigDecimal("0.00");
-        System.out.println(c.equals(a)); // false
+        BigDecimal d = new BigDecimal("0.0");
+        System.out.println(a.equals(c)); // false
+        System.out.println(b.equals(c)); // true
+        System.out.println(c.equals(d)); // false
+
+        double doubleVal = 1.745;
+        double doubleVal1 = 0.745;
+        BigDecimal bdTest = new BigDecimal(doubleVal);
+        BigDecimal bdTest1 = new BigDecimal(doubleVal1);
+        bdTest = bdTest.setScale(2, BigDecimal.ROUND_HALF_UP);
+        bdTest1 = bdTest1.setScale(2, BigDecimal.ROUND_HALF_UP);
+        System.out.println("bdTest:" + bdTest); // 1.75
+        System.out.println("bdTest1:" + bdTest1); // 0.74
+        // 使用参数为float或double的BigDecimal创建对象会丢失精度。
+        // 因此强烈建议不要使用参数为float或double的BigDecimal创建对象。
     }
 
     /**
