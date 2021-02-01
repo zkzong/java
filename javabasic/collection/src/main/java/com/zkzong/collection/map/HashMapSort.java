bc266dcb7f311d7df7cb79d22a7acdeb3a4d93a2
zongzhankui <zongzhankui@hotmail.com>
2021-02-01 08:42:15 +0800

    Map

--- javabasic/collection/src/main/java/com/zkzong/collection/map/HashMapSort.java
+++ javabasic/collection/src/main/java/com/zkzong/collection/map/HashMapSort.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by Zong on 2016/7/22.
@@ -14,10 +15,15 @@ public class HashMapSort {
     public static void main(String[] args) {
         //HashMap的值是没有顺序的，他是按照key的HashCode来实现的。
         Map<String, String> map = new HashMap<String, String>();
-        map.put("d", "d");
-        map.put("b", "b");
-        map.put("a", "a");
-        map.put("c", "c");
+        //map.put("d", "d");
+        //map.put("b", "b");
+        //map.put("a", "a");
+        //map.put("c", "c");
+        for (int i = 0; i < 10; i++) {
+            String random = UUID.randomUUID().toString();
+            System.out.println(random + "   " + random.hashCode());
+            map.put(random, random);
+        }
         System.out.println(map);
 
         List<Map.Entry<String, String>> list = new ArrayList<Map.Entry<String, String>>(map.entrySet());
