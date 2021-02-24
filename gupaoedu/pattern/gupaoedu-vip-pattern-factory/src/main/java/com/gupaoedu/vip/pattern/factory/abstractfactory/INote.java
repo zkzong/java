59186643a9d242edc797cc1cff564de5b9e2298e
zongzhankui <zongzhankui@hotmail.com>
2021-02-24 10:22:53 +0800

    delete gupaoedu

--- gupaoedu/pattern/gupaoedu-vip-pattern-factory/src/main/java/com/gupaoedu/vip/pattern/factory/abstractfactory/INote.java
+++
@@ -1,9 +0,0 @@
-package com.gupaoedu.vip.pattern.factory.abstractfactory;
-
-/**
- * 课堂笔记
- * Created by Tom.
- */
-public interface INote {
-    void edit();
-}
--- gupaoedu/pattern/gupaoedu-vip-pattern-factory/src/main/java/com/gupaoedu/vip/pattern/factory/abstractfactory/IVideo.java
+++
@@ -1,9 +0,0 @@
-package com.gupaoedu.vip.pattern.factory.abstractfactory;
-
-/**
- * 录播视频
- * Created by Tom.
- */
-public interface IVideo {
-    void record();
-}
