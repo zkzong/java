14aa8f86ecb093fac0c18f10050703f554435c2c
zongzhankui <zongzhankui@hotmail.com>
2021-04-16 17:19:03 +0800

    redis list

--- db/redis/lettuce/lettuce-spring-boot-2/src/test/java/com/zkzong/lettuce/LettuceApplicationTest.java
+++ db/redis/lettuce/lettuce-spring-boot-2/src/test/java/com/zkzong/lettuce/LettuceApplicationTest.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
@@ -32,6 +34,16 @@ public class LettuceApplicationTest {
         System.out.println(increment);
 
         lettuceConnectionFactory.getClientConfiguration();
+    }
 
+    @Test
+    public void range() {
+        ListOperations list = redisTemplate.opsForList();
+        list.leftPush("user", "name");
+        list.leftPush("user", "age");
+        list.leftPush("user", "name", "sex");
+
+        List result = list.range("user", 0, -1);
+        System.out.println(result);
     }
 }
