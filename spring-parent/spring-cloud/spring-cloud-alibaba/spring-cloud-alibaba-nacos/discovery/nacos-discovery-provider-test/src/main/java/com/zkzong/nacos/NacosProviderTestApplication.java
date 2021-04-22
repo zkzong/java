1d25383cd2b2c22a575d1c43eac90eb3ea2a4783
zongzhankui <zongzhankui@hotmail.com>
2021-04-22 11:33:31 +0800

    spring cloud alibaba

---
+++ spring-parent/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-nacos/discovery/nacos-discovery-provider-test/src/main/java/com/zkzong/nacos/NacosProviderTestApplication.java
@@ -0,0 +1,17 @@
+package com.zkzong.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: zong
+ * @Date: 2021/4/22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderTestApplication.class, args);
+    }
+}
