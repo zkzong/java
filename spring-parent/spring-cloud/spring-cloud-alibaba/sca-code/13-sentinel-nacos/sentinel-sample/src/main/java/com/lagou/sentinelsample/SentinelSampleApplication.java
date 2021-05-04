1464496aecb1b66419325b56679128e918a9fbd0
zongzhankui <zongzhankui@hotmail.com>
2021-05-04 11:22:30 +0800

    spring cloud alibaba

---
+++ spring-parent/spring-cloud/spring-cloud-alibaba/sca-code/13-sentinel-nacos/sentinel-sample/src/main/java/com/lagou/sentinelsample/SentinelSampleApplication.java
@@ -0,0 +1,19 @@
+package com.lagou.sentinelsample;
+
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SentinelSampleApplication {
+    // 注解支持的配置Bean
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelSampleApplication.class, args);
+    }
+}
