87b6b1583e338388dd0367e5860e3e64c260b491
zongzhankui <zongzhankui@hotmail.com>
2021-05-10 11:24:06 +0800

    try catch

--- javabasic/src/main/java/com/zkzong/exception/TryCatch.java
+++ javabasic/src/main/java/com/zkzong/exception/TryCatch.java
@@ -1,10 +1,13 @@
 package com.zkzong.exception;
 
+import org.junit.Test;
+
 /**
  * Created by Zong on 2016/7/14.
  */
 public class TryCatch {
-    public static void main(String[] args) {
+    @Test
+    public void test1() {
         /**
          * 异常被catch之后，后面的代码会继续执行
          */
@@ -18,5 +21,36 @@ public class TryCatch {
         }
         System.out.println(t); // 11
         System.out.println("继续执行");
+    }
+
+    @Test
+    public void test2() {
+        String s = s();
+        System.out.println(s);
+    }
+
+    private String s() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "a";
+    }
+
+    @Test
+    public void test3() {
+        String ss = ss();
+        System.out.println(ss);
+    }
+
+    private String ss() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            return "catch";
+        } finally {
+            return "finally";
+        }
     }
 }
