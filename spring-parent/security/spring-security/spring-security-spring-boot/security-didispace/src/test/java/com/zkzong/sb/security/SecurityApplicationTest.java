56f6069072dd9610a761a9915f6f551c8991a36a
zongzhankui <zongzhankui@hotmail.com>
2021-05-10 11:20:05 +0800

    controller测试用例

--- spring-parent/security/spring-security/spring-security-spring-boot/security-didispace/src/test/java/com/zkzong/sb/security/SecurityApplicationTest.java
+++ spring-parent/security/spring-security/spring-security-spring-boot/security-didispace/src/test/java/com/zkzong/sb/security/SecurityApplicationTest.java
@@ -1,15 +1,16 @@
 package com.zkzong.sb.security;
 
-import com.zkzong.sb.security.web.HelloController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,9 +24,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SecurityApplicationTest {
     private MockMvc mvc;
 
+    @Autowired
+    private WebApplicationContext context;
+
     @Before
-    public void setUp() {
-        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    public void setupMockMvc() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
--- spring-parent/spring-boot/exception-sb/exception-global/src/test/java/com/zkzong/ex/ExceptionTest.java
+++ spring-parent/spring-boot/exception-sb/exception-global/src/test/java/com/zkzong/ex/ExceptionTest.java
@@ -1,31 +1,43 @@
 package com.zkzong.ex;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ExceptionTest {
+    //@Autowired
+    private MockMvc mvc;
+
     @Autowired
-    MockMvc mockMvc;
+    private WebApplicationContext context;
+
+    @Before
+    public void setupMockMvc() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
-    void should_return_400_if_param_not_valid() throws Exception {
-        mockMvc.perform(get("/api/illegalArgumentException"))
+    public void should_return_400_if_param_not_valid() throws Exception {
+        mvc.perform(get("/api/illegalArgumentException"))
                 .andExpect(status().is(400))
                 .andExpect(jsonPath("$.message").value("参数错误!"));
     }
 
     @Test
-    void should_return_404_if_resourse_not_found() throws Exception {
-        mockMvc.perform(get("/api/resourceNotFoundException"))
+    public void should_return_404_if_resourse_not_found() throws Exception {
+        mvc.perform(get("/api/resourceNotFoundException"))
                 .andExpect(status().is(404))
                 .andExpect(jsonPath("$.message").value("Sorry, the resourse not found!"));
     }
