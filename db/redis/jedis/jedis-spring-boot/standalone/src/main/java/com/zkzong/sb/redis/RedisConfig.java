38e40262ee4dbfb0632d37f80a963fb7a74fef35
zkzong <zongzhankui@hotmail.com>
2021-05-24 10:03:59 +0800

    redis

--- db/redis/jedis/jedis-spring-boot/standalone/src/main/java/com/zkzong/sb/redis/RedisConfig.java
+++ db/redis/jedis/jedis-spring-boot/standalone/src/main/java/com/zkzong/sb/redis/RedisConfig.java
@@ -3,7 +3,6 @@ package com.zkzong.sb.redis;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -14,13 +13,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
-    @ConfigurationProperties(prefix="spring.redis")
+    @ConfigurationProperties(prefix = "spring.redis")
     JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
     }
 
     @Bean
-    public <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory factory) {
+    public <T> RedisTemplate<String, T> redisTemplate() {
         RedisTemplate<String, T> template = new RedisTemplate<String, T>();
         template.setConnectionFactory(jedisConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
