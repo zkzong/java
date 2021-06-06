131613e3f79ea1d673a3f50a226f844b736eec30
zkzong <zongzhankui@hotmail.com>
2021-06-06 11:12:08 +0800

    executor

---
+++ spring-parent/spring-boot-2/executor/src/main/java/com/zkzong/executor/ExecutorApplication.java
@@ -0,0 +1,11 @@
+package com.zkzong.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExecutorApplication.class, args);
+    }
+}
