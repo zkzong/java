c8de4e80d4052578f3dfd59fda05a471d1b84191
zkzong <zongzhankui@hotmail.com>
2021-06-16 16:00:41 +0800

    spring cloud

--- spring-parent/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-starter/src/main/java/com/zkzong/sca/controller/Controller.java
+++ spring-parent/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-starter/src/main/java/com/zkzong/sca/controller/Controller.java
@@ -1,14 +1,27 @@
 package com.zkzong.sca.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+import com.zkzong.sca.dto.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
 
-    @RequestMapping("/")
-    public String home() {
+    @GetMapping("/get")
+    public String get() {
         return "Hello World";
+    }
+
+    @PostMapping("/post")
+    public String post(@RequestParam String name) {
+        return name;
+    }
+
+    @PostMapping("/post/json")
+    public String post(@RequestBody User user) {
+        Gson gson = new Gson();
+        String s = gson.toJson(user);
+        return s;
     }
 
 }
