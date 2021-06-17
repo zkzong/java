c894bac7b6c72c6d93535f3b19db0e748c7b7fa4
zkzong <zongzhankui@hotmail.com>
2021-06-17 19:09:48 +0800

    hutool

--- thirdparty/hutool/src/main/java/com/zkzong/hutool/http/HttpTest.java
+++ thirdparty/hutool/src/main/java/com/zkzong/hutool/http/HttpTest.java
@@ -1,5 +1,8 @@
 package com.zkzong.hutool.http;
 
+import cn.hutool.http.ContentType;
+import cn.hutool.http.Header;
+import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpUtil;
 import com.google.gson.Gson;
 import org.junit.Test;
@@ -7,6 +10,9 @@ import org.junit.Test;
 import java.util.HashMap;
 
 public class HttpTest {
+
+    private String postUrl = "http://localhost:8080/post";
+    private String postJsonUrl = "http://localhost:8080/post/json";
 
     @Test
     public void get() {
@@ -16,18 +22,32 @@ public class HttpTest {
     }
 
     @Test
-    public void post() {
-        //POST请求
+    public void post1() {
         HashMap<String, Object> paramMap = new HashMap<>();
         paramMap.put("name", "long");
 
-        String result = HttpUtil.post("http://localhost:8080/post", paramMap);
+        //POST请求
+        String result = HttpUtil.post(postUrl, paramMap);
         System.out.println(result);
     }
 
     @Test
-    public void postjson() {
-        //POST请求
+    public void post2() {
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", "long");
+
+        //链式构建请求
+        String result = HttpRequest.post(postUrl)
+                .header(Header.CONTENT_TYPE, ContentType.FORM_URLENCODED.toString())//头信息，多个头信息多次调用此方法即可
+                .form(paramMap)//表单内容
+                .timeout(20000)//超时，毫秒
+                .execute().body();
+        System.out.println(result);
+    }
+
+
+    @Test
+    public void postjson1() {
         User user = new User();
         user.setId(1);
         user.setName("long");
@@ -37,7 +57,27 @@ public class HttpTest {
         Gson gson = new Gson();
         String json = gson.toJson(user);
 
-        String result = HttpUtil.post("http://localhost:8080/post/json", json);
+        // Restful请求
+        String result = HttpUtil.post(postJsonUrl, json);
+        System.out.println(result);
+    }
+
+    @Test
+    public void postjson2() {
+        User user = new User();
+        user.setId(1);
+        user.setName("long");
+        user.setAge(30);
+        user.setSex('M');
+
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
+
+        // Restful请求
+        String result = HttpRequest.post(postJsonUrl)
+                .header(Header.CONTENT_TYPE, ContentType.JSON.toString())
+                .body(json)
+                .execute().body();
         System.out.println(result);
     }
 }
