55b71708bccbec5cdbe41a4f3ae96bac9db85a1e
zkzong <zongzhankui@hotmail.com>
2021-07-04 09:32:58 +0800

    sharding jdbc 5

--- thirdparty/sharding-jdbc/sharding-jdbc-5/sharding-jdbc-5-mybatis/src/main/java/com/zkzong/sj5/repository/ShadowUserRepository.java
+++ thirdparty/sharding-jdbc/sharding-jdbc-5/sharding-jdbc-5-mybatis/src/main/java/com/zkzong/sj5/repository/ShadowUserRepository.java
@@ -1,7 +1,7 @@
-
-package com.zkzong.sj5.repository;
-
-import com.zkzong.sj5.entity.ShadowUser;
-
-public interface ShadowUserRepository extends CommonRepository<ShadowUser, Long> {
-}
+//
+//package com.zkzong.sj5.repository;
+//
+//import com.zkzong.sj5.entity.ShadowUser;
+//
+//public interface ShadowUserRepository extends CommonRepository<ShadowUser, Long> {
+//}
--- thirdparty/sharding-jdbc/sharding-jdbc-5/sharding-jdbc-5-mybatis/src/main/java/com/zkzong/sj5/service/ShadowUserServiceImpl.java
+++ thirdparty/sharding-jdbc/sharding-jdbc-5/sharding-jdbc-5-mybatis/src/main/java/com/zkzong/sj5/service/ShadowUserServiceImpl.java
@@ -1,77 +1,77 @@
-
-package com.zkzong.sj5.service;
-
-import com.zkzong.sj5.entity.ShadowUser;
-import com.zkzong.sj5.repository.ShadowUserRepository;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-@Service("shadow")
-public class ShadowUserServiceImpl implements ExampleService {
-
-    @Resource
-    private ShadowUserRepository userRepository;
-
-    @Override
-    public void initEnvironment() throws SQLException {
-        userRepository.createTableIfNotExists();
-        userRepository.truncateTable();
-    }
-
-    @Override
-    public void cleanEnvironment() throws SQLException {
-        userRepository.dropTable();
-    }
-
-    @Override
-    public void processSuccess() throws SQLException {
-        System.out.println("-------------- Process Success Begin ---------------");
-        List<Long> userIds = insertData();
-        printData();
-        deleteData(userIds);
-        printData();
-        System.out.println("-------------- Process Success Finish --------------");
-    }
-
-    private List<Long> insertData() throws SQLException {
-        System.out.println("---------------------------- Insert Data ----------------------------");
-        List<Long> result = new ArrayList<>(10);
-        for (int i = 1; i <= 10; i++) {
-            ShadowUser user = new ShadowUser();
-            user.setUserId(i);
-            user.setUserName("test_mybatis_" + i);
-            user.setPwd("pwd_mybatis_" + i);
-            user.setShadow(i % 2 == 0);
-            userRepository.insert(user);
-            result.add((long) user.getUserId());
-        }
-        return result;
-    }
-
-    @Override
-    public void processFailure() throws SQLException {
-        System.out.println("-------------- Process Failure Begin ---------------");
-        insertData();
-        System.out.println("-------------- Process Failure Finish --------------");
-        throw new RuntimeException("Exception occur for transaction test.");
-    }
-
-    private void deleteData(final List<Long> userIds) throws SQLException {
-        System.out.println("---------------------------- Delete Data ----------------------------");
-        for (Long each : userIds) {
-            userRepository.delete(each);
-        }
-    }
-
-    @Override
-    public void printData() throws SQLException {
-        System.out.println("---------------------------- Print User Data -----------------------");
-        for (Object each : userRepository.selectAll()) {
-            System.out.println(each);
-        }
-    }
-}
+//
+//package com.zkzong.sj5.service;
+//
+//import com.zkzong.sj5.entity.ShadowUser;
+//import com.zkzong.sj5.repository.ShadowUserRepository;
+//import org.springframework.stereotype.Service;
+//
+//import javax.annotation.Resource;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Service("shadow")
+//public class ShadowUserServiceImpl implements ExampleService {
+//
+//    @Resource
+//    private ShadowUserRepository userRepository;
+//
+//    @Override
+//    public void initEnvironment() throws SQLException {
+//        userRepository.createTableIfNotExists();
+//        userRepository.truncateTable();
+//    }
+//
+//    @Override
+//    public void cleanEnvironment() throws SQLException {
+//        userRepository.dropTable();
+//    }
+//
+//    @Override
+//    public void processSuccess() throws SQLException {
+//        System.out.println("-------------- Process Success Begin ---------------");
+//        List<Long> userIds = insertData();
+//        printData();
+//        deleteData(userIds);
+//        printData();
+//        System.out.println("-------------- Process Success Finish --------------");
+//    }
+//
+//    private List<Long> insertData() throws SQLException {
+//        System.out.println("---------------------------- Insert Data ----------------------------");
+//        List<Long> result = new ArrayList<>(10);
+//        for (int i = 1; i <= 10; i++) {
+//            ShadowUser user = new ShadowUser();
+//            user.setUserId(i);
+//            user.setUserName("test_mybatis_" + i);
+//            user.setPwd("pwd_mybatis_" + i);
+//            user.setShadow(i % 2 == 0);
+//            userRepository.insert(user);
+//            result.add((long) user.getUserId());
+//        }
+//        return result;
+//    }
+//
+//    @Override
+//    public void processFailure() throws SQLException {
+//        System.out.println("-------------- Process Failure Begin ---------------");
+//        insertData();
+//        System.out.println("-------------- Process Failure Finish --------------");
+//        throw new RuntimeException("Exception occur for transaction test.");
+//    }
+//
+//    private void deleteData(final List<Long> userIds) throws SQLException {
+//        System.out.println("---------------------------- Delete Data ----------------------------");
+//        for (Long each : userIds) {
+//            userRepository.delete(each);
+//        }
+//    }
+//
+//    @Override
+//    public void printData() throws SQLException {
+//        System.out.println("---------------------------- Print User Data -----------------------");
+//        for (Object each : userRepository.selectAll()) {
+//            System.out.println(each);
+//        }
+//    }
+//}
