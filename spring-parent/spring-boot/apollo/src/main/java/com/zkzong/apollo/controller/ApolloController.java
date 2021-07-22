2d117fa77d351a16994a04bec2e0c1a13e1e56dd
zkzong <zongzhankui@hotmail.com>
2021-07-22 10:19:15 +0800

    apollo

--- spring-parent/spring-boot/apollo/src/main/java/com/zkzong/apollo/controller/ApolloController.java
+++ spring-parent/spring-boot/apollo/src/main/java/com/zkzong/apollo/controller/ApolloController.java
@@ -1,6 +1,9 @@
 package com.zkzong.apollo.controller;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,25 @@ public class ApolloController {
     private String configInfo;
 
     @RequestMapping("/getConfigInfo")
-    public String getConfigInfo(){
+    public String getConfigInfo() {
         return configInfo;
+    }
+
+    @GetMapping("/api")
+    public void api() {
+        // 默认命名空间
+        Config appConfig = ConfigService.getAppConfig();
+        String configInfo = appConfig.getProperty("config_info", "");
+        System.out.println(configInfo);
+
+        // 公共命名空间
+        Config publicConfig = ConfigService.getConfig("ns-public");
+        String aaa = publicConfig.getProperty("aaa", "");
+        System.out.println(aaa);
+
+        // 私有命名空间
+        Config privateConfig = ConfigService.getConfig("ns-private");
+        String bbb = privateConfig.getProperty("bbb", "");
+        System.out.println(bbb);
     }
 }
