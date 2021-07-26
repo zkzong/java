bbf10c25245486860f1079437cdc1f59efb07e49
zkzong <zongzhankui@hotmail.com>
2021-07-26 09:44:49 +0800

    delete license

--- thirdparty/sharding-jdbc/sharding-jdbc-spring-boot-2/src/main/java/com/zkzong/ss/service/OrderServiceImpl.java
+++ thirdparty/sharding-jdbc/sharding-jdbc-spring-boot-2/src/main/java/com/zkzong/ss/service/OrderServiceImpl.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.zkzong.ss.service;
 
 import com.zkzong.ss.entity.AddressEntity;
@@ -35,13 +18,13 @@ import java.util.List;
 @Service
 @Primary
 public class OrderServiceImpl implements ExampleService {
-    
+
     @Resource
     private OrderRepository orderRepository;
-    
+
     @Resource
     private OrderItemRepository orderItemRepository;
-    
+
     @Resource
     private AddressRepository addressRepository;
 
@@ -54,11 +37,11 @@ public class OrderServiceImpl implements ExampleService {
             addressRepository.insert(entity);
         }
     }
-    
+
     @Override
     public void cleanEnvironment() {
     }
-    
+
     @Override
     @Transactional
     public void processSuccess() throws SQLException {
@@ -69,7 +52,7 @@ public class OrderServiceImpl implements ExampleService {
         printData();
         System.out.println("-------------- Process Success Finish --------------");
     }
-    
+
     @Override
     @Transactional
     public void processFailure() throws SQLException {
@@ -105,7 +88,7 @@ public class OrderServiceImpl implements ExampleService {
             orderItemRepository.delete(each);
         }
     }
-    
+
     @Override
     public void printData() throws SQLException {
         System.out.println("---------------------------- Print Order Data -----------------------");
--- thirdparty/sharding-jdbc/sharding-jdbc-spring-boot-2/src/main/java/com/zkzong/ss/service/UserServiceImpl.java
+++ thirdparty/sharding-jdbc/sharding-jdbc-spring-boot-2/src/main/java/com/zkzong/ss/service/UserServiceImpl.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.zkzong.ss.service;
 
 import com.zkzong.ss.entity.UserEntity;
@@ -29,18 +12,18 @@ import java.util.List;
 
 @Service("encrypt")
 public class UserServiceImpl implements ExampleService {
-    
+
     @Resource
     private UserRepository userRepository;
-    
+
     @Override
     public void initEnvironment() {
     }
-    
+
     @Override
     public void cleanEnvironment() {
     }
-    
+
     @Override
     @Transactional
     public void processSuccess() throws SQLException {
@@ -51,7 +34,7 @@ public class UserServiceImpl implements ExampleService {
         printData();
         System.out.println("-------------- Process Success Finish --------------");
     }
-    
+
     @Override
     @Transactional
     public void processFailure() throws SQLException {
@@ -60,7 +43,7 @@ public class UserServiceImpl implements ExampleService {
         System.out.println("-------------- Process Failure Finish --------------");
         throw new RuntimeException("Exception occur for transaction test.");
     }
-    
+
     private List<Long> insertData() throws SQLException {
         System.out.println("---------------------------- Insert Data ----------------------------");
         List<Long> result = new ArrayList<>(10);
@@ -74,14 +57,14 @@ public class UserServiceImpl implements ExampleService {
         }
         return result;
     }
-    
+
     private void deleteData(final List<Long> userIds) {
 //        System.out.println("---------------------------- Delete Data ----------------------------");
 //        for (Long each : userIds) {
 //            userRepository.delete(each);
 //        }
     }
-    
+
     @Override
     public void printData() {
         System.out.println("---------------------------- Print User Data -----------------------");
