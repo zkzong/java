ee44323158bfbc3192bb77172d498326c3e1ac23
zkzong <zongzhankui@hotmail.com>
2021-07-26 17:45:32 +0800

    elasticjob

--- job-scheduler/elastic-job/elastic-job-3-sb/src/main/java/com/zkzong/elasticjob/controller/OneOffJobController.java
+++ job-scheduler/elastic-job/elastic-job-3-sb/src/main/java/com/zkzong/elasticjob/controller/OneOffJobController.java
@@ -1,8 +1,6 @@
 package com.zkzong.elasticjob.controller;
 
 import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.OneOffJobBootstrap;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +14,17 @@ public class OneOffJobController {
     @Resource(name = "manualScriptJobBean")
     private OneOffJobBootstrap manualScriptJob;
 
-    @Autowired
-    @Qualifier(value = "occurErrorNoticeDingtalkBean")
-    private OneOffJobBootstrap occurErrorNoticeDingtalkJob;
+    //@Autowired
+    //@Qualifier(value = "occurErrorNoticeDingtalkBean")
+    //private OneOffJobBootstrap occurErrorNoticeDingtalkJob;
 
-    @Autowired
-    @Qualifier(value = "occurErrorNoticeWechatBean")
-    private OneOffJobBootstrap occurErrorNoticeWechatJob;
+    //@Autowired
+    //@Qualifier(value = "occurErrorNoticeWechatBean")
+    //private OneOffJobBootstrap occurErrorNoticeWechatJob;
 
-    @Autowired
-    @Qualifier(value = "occurErrorNoticeEmailBean")
-    private OneOffJobBootstrap occurErrorNoticeEmailJob;
+    //@Autowired
+    //@Qualifier(value = "occurErrorNoticeEmailBean")
+    //private OneOffJobBootstrap occurErrorNoticeEmailJob;
 
     @GetMapping("/execute/manualScriptJob")
     public String executeManualScriptJob() {
@@ -34,21 +32,21 @@ public class OneOffJobController {
         return RES_TEXT;
     }
 
-    @GetMapping("/execute/occurErrorNoticeDingtalkJob")
-    public String executeOneOffJob() {
-        occurErrorNoticeDingtalkJob.execute();
-        return RES_TEXT;
-    }
+    //@GetMapping("/execute/occurErrorNoticeDingtalkJob")
+    //public String executeOneOffJob() {
+    //    occurErrorNoticeDingtalkJob.execute();
+    //    return RES_TEXT;
+    //}
 
-    @GetMapping("/execute/occurErrorNoticeWechatJob")
-    public String executeOccurErrorNoticeWechatJob() {
-        occurErrorNoticeWechatJob.execute();
-        return RES_TEXT;
-    }
+    //@GetMapping("/execute/occurErrorNoticeWechatJob")
+    //public String executeOccurErrorNoticeWechatJob() {
+    //    occurErrorNoticeWechatJob.execute();
+    //    return RES_TEXT;
+    //}
 
-    @GetMapping("/execute/occurErrorNoticeEmailJob")
-    public String executeOccurErrorNoticeEmailJob() {
-        occurErrorNoticeEmailJob.execute();
-        return RES_TEXT;
-    }
+    //@GetMapping("/execute/occurErrorNoticeEmailJob")
+    //public String executeOccurErrorNoticeEmailJob() {
+    //    occurErrorNoticeEmailJob.execute();
+    //    return RES_TEXT;
+    //}
 }
