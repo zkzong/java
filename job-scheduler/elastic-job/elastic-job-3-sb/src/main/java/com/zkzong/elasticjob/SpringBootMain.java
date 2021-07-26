ae6957fe01798a764fcbfb15e4f9e84e0ec0de83
zkzong <zongzhankui@hotmail.com>
2021-07-26 09:44:09 +0800

    elasticjob

---
+++ job-scheduler/elastic-job/elastic-job-3-sb/src/main/java/com/zkzong/elasticjob/SpringBootMain.java
@@ -0,0 +1,16 @@
+package com.zkzong.elasticjob;
+
+import com.zkzong.elasticjob.zk.EmbedZookeeperServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMain {
+
+    // CHECKSTYLE:OFF
+    public static void main(final String[] args) {
+        // CHECKSTYLE:ON
+        EmbedZookeeperServer.start(6181);
+        SpringApplication.run(SpringBootMain.class, args);
+    }
+}
