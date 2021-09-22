4610fe33554ed001aa3d3f769590732e1a4c92ec
zkzong <zongzhankui@hotmail.com>
2021-09-22 20:09:03 +0800

    mybatis plus

--- db/mysql/mybatis-plus/mybatis-plus-example/src/main/java/com/zkzong/mp/MybatisPlusApplication.java
+++ db/mysql/mybatis-plus/mybatis-plus-example/src/main/java/com/zkzong/mp/MybatisPlusApplication.java
@@ -1,11 +1,9 @@
 package com.zkzong.mp;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.zkzong.mp.mapper")
 public class MybatisPlusApplication {
 
     public static void main(String[] args) {
