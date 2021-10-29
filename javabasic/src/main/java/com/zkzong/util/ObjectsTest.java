0c4cb2f82a2c4530b4effcc6825463a7af0fb5ce
zkzong <zongzhankui@hotmail.com>
2021-10-29 14:03:47 +0800

    Objects

---
+++ javabasic/src/main/java/com/zkzong/util/ObjectsTest.java
@@ -0,0 +1,18 @@
+package com.zkzong.util;
+
+import org.junit.Test;
+
+import java.util.Objects;
+
+/**
+ * @Author: zong
+ * @Date: 2021/10/26
+ */
+public class ObjectsTest {
+
+    @Test
+    public void test() {
+        Objects.isNull(null);
+    }
+
+}
