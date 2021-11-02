fc2bb0dc0b5686a1d46bd43fe3c56ee8a766ec7e
zkzong <zongzhankui@hotmail.com>
2021-11-02 21:45:16 +0800

    easayexcel

---
+++ thirdparty/easyexcel/src/main/java/com/zkzong/easyexcel/EasyExcelApplication.java
@@ -0,0 +1,15 @@
+package com.zkzong.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: zong
+ * @Date: 2021/10/30
+ */
+@SpringBootApplication
+public class EasyExcelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyExcelApplication.class, args);
+    }
+}
