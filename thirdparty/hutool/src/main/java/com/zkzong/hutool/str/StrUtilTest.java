2a64a686237d805f5c9a86b5b07b28a7490e3f2b
zkzong <zongzhankui@hotmail.com>
2021-11-30 23:02:31 +0800

    hutool

--- thirdparty/hutool/src/main/java/com/zkzong/hutool/str/StrUtilTest.java
+++ thirdparty/hutool/src/main/java/com/zkzong/hutool/str/StrUtilTest.java
@@ -11,7 +11,8 @@ public class StrUtilTest {
 
     @Test
     public void a() {
-        String s = "/a/b/c/d/e";
+        String s = "/aaaaa/b/c/d/e";
+        // 从 坐标 3 开始查找/出现的位置
         int i = StrUtil.indexOf(s, '/', 3);
         System.out.println(i);
     }
@@ -20,6 +21,7 @@ public class StrUtilTest {
     @Test
     public void b() {
         String s = "= service.add(user);";
+        // 查找 service 第1次出现的位置
         int i = StrUtil.ordinalIndexOf(s, "service", 1);
         System.out.println(i);
         int j = StrUtil.indexOf(s, '(', i);
@@ -27,6 +29,21 @@ public class StrUtilTest {
 
         String sub = StrUtil.sub(s, i, j);
         System.out.println(sub);
+    }
+
+    @Test
+    public void c() {
+        String s = "a.b.c.d.e";
+        // 查找 . 最后1次出现位置
+        int i = StrUtil.lastIndexOfIgnoreCase(s, ".");
+        System.out.println(i);
+
+        String s1 = s.substring(0, i);
+        System.out.println(s1);
+        String s2 = s.substring(i + 1, s.length());
+        System.out.println(s2);
 
     }
+
+
 }
