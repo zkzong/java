e77de969e3b13e61fcfa8465f51f9ea04d2db84e
zkzong <zongzhankui@hotmail.com>
2021-12-07 17:42:23 +0800

    String

--- javabasic/string/src/main/java/com/zkzong/string/StringTest.java
+++ javabasic/string/src/main/java/com/zkzong/string/StringTest.java
@@ -16,4 +16,33 @@ public class StringTest {
         ss = ss + "!";
         System.out.print(ss);
     }
+
+    @Test
+    public void contains() {
+        String s = "a.b.c.d";
+        boolean contains = s.contains(".b.");
+        System.out.println(contains);
+    }
+
+    @Test
+    public void matches1() {
+        String s = "String a = service.getString()";
+        boolean matches = s.matches("(.*)service(.*)");
+        System.out.println(matches);
+    }
+
+    @Test
+    public void matches2() {
+        String Str = new String("www.runoob.com");
+
+        System.out.print("返回值 :");
+        System.out.println(Str.matches("(.*)runoob(.*)"));
+
+        System.out.print("返回值 :");
+        System.out.println(Str.matches("(.*)google(.*)"));
+
+        System.out.print("返回值 :");
+        System.out.println(Str.matches("www(.*)"));
+    }
+
 }
