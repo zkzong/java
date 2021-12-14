dd1d7dfaf1f8f1e51fc3d74055a6791eac7f1202
zongzhankui <zongzhankui@hotmail.com>
2021-12-14 17:11:28 +0800

    sharding

---
+++ thirdparty/sharding-jdbc/sharding-jdbc-5/sharding-jdbc-5-mybatis/src/test/resources/sql/table.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS t_order_0 (
+    order_id   BIGINT AUTO_INCREMENT,
+    user_id    INT    NOT NULL,
+    address_id BIGINT NOT NULL,
+    status     VARCHAR(50),
+    PRIMARY KEY (order_id)
+);
+CREATE TABLE IF NOT EXISTS t_order_0 (
+    order_id   BIGINT AUTO_INCREMENT,
+    user_id    INT    NOT NULL,
+    address_id BIGINT NOT NULL,
+    status     VARCHAR(50),
+PRIMARY KEY (order_id)
+);
+CREATE TABLE IF NOT EXISTS t_order_item_0 (
+    order_item_id BIGINT AUTO_INCREMENT,
+    order_id      BIGINT,
+    user_id       INT NOT NULL,
+    status        VARCHAR(50),
+    PRIMARY KEY (order_item_id)
+);
+CREATE TABLE IF NOT EXISTS t_order_item_1 (
+    order_item_id BIGINT AUTO_INCREMENT,
+    order_id      BIGINT,
+    user_id       INT NOT NULL,
+    status        VARCHAR(50),
+    PRIMARY KEY (order_item_id)
+);
