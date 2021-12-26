af7490b46af6fac786a28d8e63f5894e6a251647
zongzhankui <zongzhankui@hotmail.com>
2021-12-26 23:17:37 +0800

    loadbalancer

---
+++ spring-parent/spring-cloud/spring-cloud-alibaba/loadbalancer/loadbalancer-consumer/src/main/java/com/zkzong/lb/RandomLoadBalancer.java
@@ -0,0 +1,51 @@
+package com.zkzong.lb;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.reactive.DefaultResponse;
+import org.springframework.cloud.client.loadbalancer.reactive.EmptyResponse;
+import org.springframework.cloud.client.loadbalancer.reactive.Request;
+import org.springframework.cloud.client.loadbalancer.reactive.Response;
+import org.springframework.cloud.loadbalancer.core.NoopServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomLoadBalancer implements ReactorServiceInstanceLoadBalancer {
+
+    private ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider;
+
+    private final String serviceId;
+
+    private final Random random;
+
+    public RandomLoadBalancer(
+            ObjectProvider<ServiceInstanceListSupplier> serviceInstanceListSupplierProvider, String serviceId) {
+        this.serviceInstanceListSupplierProvider = serviceInstanceListSupplierProvider;
+        this.serviceId = serviceId;
+        this.random = new Random();
+    }
+
+    @Override
+    public Mono<Response<ServiceInstance>> choose(Request request) {
+        ServiceInstanceListSupplier supplier = serviceInstanceListSupplierProvider
+                .getIfAvailable(NoopServiceInstanceListSupplier::new);
+        return supplier.get().next().map(this::getInstanceResponse);
+    }
+
+    private Response<ServiceInstance> getInstanceResponse(
+            List<ServiceInstance> instances) {
+        if (instances.isEmpty()) {
+            return new EmptyResponse();
+        }
+        int randomNum = random.nextInt(instances.size());
+        System.out.println("random: " + randomNum);
+        ServiceInstance instance = instances.get(randomNum);
+
+        return new DefaultResponse(instance);
+    }
+
+}
---
+++ spring-parent/spring-cloud/spring-cloud-alibaba/loadbalancer/loadbalancer-provider/src/main/java/com/zkzong/lb/NacosProvider4LoadBalance.java
@@ -0,0 +1,27 @@
+package com.zkzong.lb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@SpringBootApplication
+public class NacosProvider4LoadBalance {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProvider4LoadBalance.class, args);
+    }
+
+    @RestController
+    class EchoController {
+
+        @GetMapping("/")
+        public String echo(HttpServletRequest request) {
+            return request.getLocalAddr() + ":" + request.getLocalPort();
+        }
+
+    }
+
+}
