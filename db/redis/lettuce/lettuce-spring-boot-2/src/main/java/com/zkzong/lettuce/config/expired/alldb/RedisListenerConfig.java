40b075b0c9604dadbb3b282231cd83e5c8d43781
zongzhankui <zongzhankui@hotmail.com>
2022-01-04 21:21:23 +0800

    redis

---
+++ db/redis/lettuce/lettuce-spring-boot-2/src/main/java/com/zkzong/lettuce/config/expired/alldb/RedisListenerConfig.java
@@ -0,0 +1,22 @@
+package com.zkzong.lettuce.config.expired.alldb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @Author: zong
+ * @Date: 2022/1/4
+ */
+@Configuration
+public class RedisListenerConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+}
