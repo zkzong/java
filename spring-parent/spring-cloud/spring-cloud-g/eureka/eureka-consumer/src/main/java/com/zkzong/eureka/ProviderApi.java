4df98ea2b1ba187f54e3dfa64cda85af702cdd46
zongzhankui <zongzhankui@hotmail.com>
2022-01-20 14:13:33 +0800

    eureka

--- spring-parent/spring-cloud/spring-cloud-g/eureka/eureka-consumer/src/main/java/com/zkzong/eureka/ProviderApi.java
+++ spring-parent/spring-cloud/spring-cloud-g/eureka/eureka-consumer/src/main/java/com/zkzong/eureka/ProviderApi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("my-provider")
+@FeignClient("eureka-provider")
 public interface ProviderApi {
 
     @RequestMapping("/echo1")
