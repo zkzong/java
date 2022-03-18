751fafe74e0414f6652e153d3c1c68f683185176
zongzhankui <zongzhankui@hotmail.com>
2022-03-18 09:14:33 +0800

    validator

--- spring-parent/spring-boot/validator/src/main/java/com/zkzong/sb/controller/ValidatorController.java
+++ spring-parent/spring-boot/validator/src/main/java/com/zkzong/sb/controller/ValidatorController.java
@@ -3,6 +3,7 @@ package com.zkzong.sb.controller;
 import com.zkzong.sb.dto.RspDTO;
 import com.zkzong.sb.dto.UserDTO;
 import com.zkzong.sb.dto.UserReq;
+import com.zkzong.sb.dto.assist.Create;
 import com.zkzong.sb.dto.assist.Update;
 import com.zkzong.sb.service.UserService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +13,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.ConstraintViolation;
+import java.util.Set;
+
 @RestController
 @Slf4j
 public class ValidatorController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private javax.validation.Validator globalValidator;
 
     /**
      * 走参数校验注解
@@ -32,6 +39,12 @@ public class ValidatorController {
         return RspDTO.success();
     }
 
+    /**
+     * 嵌套校验
+     *
+     * @param req
+     * @return
+     */
     @PostMapping("/create/list")
     public RspDTO save(@RequestBody @Validated UserReq req) {
         log.info("入参：{}", req);
@@ -50,6 +63,26 @@ public class ValidatorController {
     @PostMapping("/update/groups")
     public RspDTO update(@RequestBody @Validated(Update.class) UserDTO userDTO) {
         userService.updateById(userDTO);
+        return RspDTO.success();
+    }
+
+    /**
+     * 编程式校验
+     *
+     * @param userDTO
+     * @return
+     */
+    @PostMapping("/create/code")
+    public RspDTO save1(@RequestBody @Validated UserDTO userDTO) {
+        Set<ConstraintViolation<UserDTO>> validate = globalValidator.validate(userDTO, Create.class);
+
+        if (validate.isEmpty()) {
+
+        } else {
+            for (ConstraintViolation<UserDTO> userDTOConstraintViolation : validate) {
+                System.out.println(userDTOConstraintViolation);
+            }
+        }
         return RspDTO.success();
     }
 
