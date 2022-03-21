3df49849b387279412297fd5c2df93e53b56d817
zongzhankui <zongzhankui@hotmail.com>
2022-03-21 15:48:48 +0800

    api

---
+++ spring-parent/spring-boot-2/api/src/main/java/com/zkzong/sb2/common/ResponseResultHandler.java
@@ -0,0 +1,47 @@
+package com.zkzong.sb2.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: zong
+ * @Date: 2022/3/21
+ */
+@Slf4j
+@ControllerAdvice
+public class ResponseResultHandler implements ResponseBodyAdvice<Object> {
+    // 标记名称
+    public static final String RESPONSE_RESULT_ANN = "RESPONSE-RESULT-ANN";
+
+    // 是否请求 包含了 包装注解 标记，没有就直接返回，不需要重写返回体
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = sra.getRequest();
+        // 判断请求 是否有包装标记
+        ResponseResult responseResultAnn = (ResponseResult) request.getAttribute(RESPONSE_RESULT_ANN);
+        return responseResultAnn == null ? false : true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        log.info("进入 返回体 重写格式 处理中.....");
+
+        //if (body instanceof ErrorResult) {
+        //    log.info("返回值 异常 作包装 处理中.....");
+        //    ErrorResult errorResult = body;
+        //    return Result.failure(errorResult.getCode(), errorResult.getMessage(), errorResult.getErrors());
+        //}
+        return Result.success(body);
+    }
+}
---
+++ spring-parent/spring-boot-2/api/src/main/java/com/zkzong/sb2/common/Result.java
@@ -0,0 +1,77 @@
+package com.zkzong.sb2.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author: zong
+ * @Date: 2022/3/21
+ */
+@Data
+public class Result implements Serializable {
+
+    private Integer code;
+
+    private String message;
+
+    private ResultCode resultCode;
+
+    private Object data;
+
+    public Result() {
+    }
+
+    public Result(ResultCode resultCode, Object data) {
+        this.code = resultCode.getCode();
+        this.message = resultCode.getMessage();
+        this.data = data;
+    }
+
+    /**
+     * 返回成功
+     *
+     * @return
+     */
+    public static Result success() {
+        Result result = new Result();
+        result.setResultCode(ResultCode.SUCCESS);
+        return result;
+    }
+
+    /**
+     * 返回成功
+     *
+     * @return
+     */
+    public static Result success(Object data) {
+        Result result = new Result();
+        result.setResultCode(ResultCode.SUCCESS);
+        result.setData(data);
+        return result;
+    }
+
+    /**
+     * 返回失败
+     *
+     * @return
+     */
+    public static Result failure(ResultCode resultCode) {
+        Result result = new Result();
+        result.setResultCode(resultCode);
+        return result;
+    }
+
+    /**
+     * 返回失败
+     *
+     * @return
+     */
+    public static Result success(ResultCode resultCode, Object data) {
+        Result result = new Result();
+        result.setResultCode(resultCode);
+        result.setData(data);
+        return result;
+    }
+
+}
