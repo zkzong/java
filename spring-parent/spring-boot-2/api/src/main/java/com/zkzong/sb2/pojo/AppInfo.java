4c675378885962689f3b7a55c2f4792ac52b1c8b
zongzhankui <zongzhankui@hotmail.com>
2022-03-21 10:11:45 +0800

    api

---
+++ spring-parent/spring-boot-2/api/src/main/java/com/zkzong/sb2/pojo/AppInfo.java
@@ -0,0 +1,24 @@
+package com.zkzong.sb2.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: zong
+ * @Date: 2022/3/20
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppInfo {
+    /**
+     * App id
+     */
+    private String appId;
+    /**
+     * API 秘钥
+     */
+    private String key;
+}
+
