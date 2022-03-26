53167322f7cd64c9a7ee2f5b282b702614818253
zongzhankui <zongzhankui@hotmail.com>
2022-03-26 13:02:46 +0800

    jvm

---
+++ javabasic/jvm/src/main/java/com/zkzong/jvm/JvmThread.java
@@ -0,0 +1,40 @@
+package com.zkzong.jvm;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @Author: zong
+ * @Date: 2022/3/26
+ */
+public class JvmThread {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            List<byte[]> list = new ArrayList<byte[]>();
+            while (true) {
+                System.out.println(new Date().toString() + Thread.currentThread() + "==");
+                byte[] b = new byte[1024 * 1024 * 1];
+                list.add(b);
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        // 线程二
+        new Thread(() -> {
+            while (true) {
+                System.out.println(new Date().toString() + Thread.currentThread() + "==");
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
+
