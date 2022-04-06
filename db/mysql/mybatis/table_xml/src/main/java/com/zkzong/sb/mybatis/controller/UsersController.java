3cab51aae5bfd27bfdfaf35d346f86a210922158
zongzhankui <zongzhankui@hotmail.com>
2022-04-06 19:58:49 +0800

    mybatis

---
+++ db/mysql/mybatis/table_xml/src/main/java/com/zkzong/sb/mybatis/controller/UsersController.java
@@ -0,0 +1,23 @@
+package com.zkzong.sb.mybatis.controller;
+
+import com.zkzong.sb.mybatis.domain.Users;
+import com.zkzong.sb.mybatis.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UsersService usersService;
+
+    @PostMapping("/insert")
+    public String insert(@RequestBody Users users) {
+        usersService.insertAndGetId(users);
+        return "success";
+    }
+}
