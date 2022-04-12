fb61d9c7261f8eb79c9d3d62c12c649172ef9f59
zongzhankui <zongzhankui@hotmail.com>
2022-04-12 13:22:30 +0800

    nacos

--- spring-parent/spring-cloud/spring-cloud-g/nacos-sc/nacos-sc-discovery/nacos-sc-provider/src/main/java/com/zkzong/nacos/controller/EchoController.java
+++ spring-parent/spring-cloud/spring-cloud-g/nacos-sc/nacos-sc-discovery/nacos-sc-provider/src/main/java/com/zkzong/nacos/controller/EchoController.java
@@ -7,16 +7,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class EchoController {
 
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
-    public String echo(@PathVariable String string) {
+    public String echo(@PathVariable String string) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
         return "Hello Nacos Discovery " + string;
     }
 
+    // OpenFeign 默认的接口超时时间为 1s，可以修改调用方ribbon的配置
     @PostMapping("echo1")
-    public String echo1(@RequestParam String name) {
+    public String echo1(@RequestParam String name) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
         return "echo, " + name;
     }
 
