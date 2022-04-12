e81e9b6e507209e0348194c69b2ea4e025f8310f
zongzhankui <zongzhankui@hotmail.com>
2022-04-12 12:51:52 +0800

    nacos

--- spring-parent/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-nacos/discovery/nacos-discovery-provider/src/main/java/com/zkzong/nacos/controller/EchoController.java
+++ spring-parent/spring-cloud/spring-cloud-alibaba/spring-cloud-alibaba-nacos/discovery/nacos-discovery-provider/src/main/java/com/zkzong/nacos/controller/EchoController.java
@@ -1,9 +1,17 @@
 package com.zkzong.nacos.controller;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * @Author: zong
@@ -11,8 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class EchoController {
-    @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+
+    @NacosInjected
+    private NamingService namingService;
+
+    @RequestMapping(value = "/echo/{string}", method = GET)
     public String echo(@PathVariable String string) {
         return "Hello Nacos Discovery " + string;
+    }
+
+    // todo NullPointerException
+    @RequestMapping(value = "/get", method = GET)
+    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+        return namingService.getAllInstances(serviceName);
     }
 }
--- spring-parent/spring-cloud/spring-cloud-g/nacos-sc/nacos-sc-discovery/nacos-sc-provider/src/main/java/com/zkzong/nacos/controller/EchoController.java
+++ spring-parent/spring-cloud/spring-cloud-g/nacos-sc/nacos-sc-discovery/nacos-sc-provider/src/main/java/com/zkzong/nacos/controller/EchoController.java
@@ -1,17 +1,15 @@
 package com.zkzong.nacos.controller;
 
-import com.alibaba.nacos.api.annotation.NacosInjected;
-import com.alibaba.nacos.api.exception.NacosException;
-import com.alibaba.nacos.api.naming.NamingService;
-import com.alibaba.nacos.api.naming.pojo.Instance;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
+
     @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
     public String echo(@PathVariable String string) {
         return "Hello Nacos Discovery " + string;
@@ -20,15 +18,6 @@ public class EchoController {
     @PostMapping("echo1")
     public String echo1(@RequestParam String name) {
         return "echo, " + name;
-    }
-
-    @NacosInjected
-    private NamingService namingService;
-
-    @RequestMapping(value = "/get", method = GET)
-    @ResponseBody
-    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
-        return namingService.getAllInstances(serviceName);
     }
 
 }
