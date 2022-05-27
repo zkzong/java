1354e2eb44be977297ce1b64a8f5f6b3345072e5
zkzong <zongzhankui@hotmail.com>
2022-05-27 22:10:17 +0800

    mybatis

--- db/mysql/mybatis/table_xml/src/main/java/com/zkzong/sb/mybatis/mapper/CityDao.java
+++ db/mysql/mybatis/table_xml/src/main/java/com/zkzong/sb/mybatis/mapper/CityDao.java
@@ -3,6 +3,7 @@ package com.zkzong.sb.mybatis.mapper;
 import com.zkzong.sb.mybatis.domain.City;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.cursor.Cursor;
 
 /**
  * Created by Zong on 2017/6/5.
@@ -10,4 +11,6 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface CityDao {
     City findByName(@Param("cityName") String cityName);
+
+    Cursor<City> scan(@Param("limit") int limit);
 }
