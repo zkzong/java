32ac715c49b10ac192c85320a4b02d4dd4153d41
zongzhankui <zongzhankui@hotmail.com>
2022-06-17 17:45:38 +0800

    javabasic

---
+++ javabasic/src/main/java/com/zkzong/PassByValue.java
@@ -0,0 +1,38 @@
+package com.zkzong;
+
+class Person {
+    private String name;
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class PassByValue {
+    public static void main(String[] args) {
+        Person xiaoZhang = new Person("小张");
+        Person xiaoLi = new Person("小李");
+        swap(xiaoZhang, xiaoLi);
+        System.out.println("xiaoZhang:" + xiaoZhang.getName());
+        System.out.println("xiaoLi:" + xiaoLi.getName());
+    }
+
+    public static void swap(Person person1, Person person2) {
+        Person temp = person1;
+        person1 = person2;
+        person2 = temp;
+        System.out.println("person1:" + person1.getName());
+        System.out.println("person2:" + person2.getName());
+    }
+}
+
+
+
