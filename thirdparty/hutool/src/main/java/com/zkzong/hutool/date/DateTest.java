254e07e9d0afc3684e23cb08d63f5d43c1bf2b04
zongzhankui <zongzhankui@hotmail.com>
2022-08-02 16:07:38 +0800

    hutool

--- thirdparty/hutool/src/main/java/com/zkzong/hutool/date/DateTest.java
+++ thirdparty/hutool/src/main/java/com/zkzong/hutool/date/DateTest.java
@@ -21,27 +21,38 @@ public class DateTest {
 
         // between
         final long between = DateUtil.between(sdf.parse("2021-09-25 20:00:00"), new Date(), DateUnit.DAY);
-        System.out.println(between);
+        System.out.println("相隔天数：" + between);
 
         // compare
         final long compare = DateUtil.compare(sdf.parse("2021-09-25 20:00:00"), new Date());
-        System.out.println(compare);
+        System.out.println("日期比较：" + compare);
 
         // yesterday
         final Date date = DateUtil.yesterday().toJdkDate();
-        System.out.println(date);
+        System.out.println("昨天：" + date);
+
+        // tomorrow
+        final Date tomorrow = DateUtil.tomorrow().toJdkDate();
+        System.out.println("明天：" + tomorrow);
+
+        // 下一天
+        DateTime nextDay = DateUtil.offsetDay(new Date(), 1);
+        System.out.println("下一天：" + nextDay);
+
+        DateTime beginOfDay = DateUtil.beginOfDay(new Date());
+        System.out.println("当天开始时间：" + beginOfDay);
 
         // 月初/月末
         DateTime beginOfMonth = DateUtil.beginOfMonth(new Date());
-        System.out.println(beginOfMonth);
+        System.out.println("月初：" + beginOfMonth);
         DateTime endOfMonth = DateUtil.endOfMonth(new Date());
-        System.out.println(endOfMonth);
+        System.out.println("月末：" + endOfMonth);
 
         // 下个月月初
         DateTime dateTime = DateUtil.nextMonth();
-        System.out.println(dateTime);
+        System.out.println("下个月：" + dateTime);
         DateTime begin = DateUtil.beginOfMonth(dateTime);
-        System.out.println(begin);
+        System.out.println("月初：" + begin);
     }
 
 }
