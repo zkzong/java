898a9d48c24d4e1d4c3a928f587dded5f4d5f68c
zongzhankui <zongzhankui@hotmail.com>
2022-08-25 20:17:28 +0800

    guava

--- thirdparty/guava/src/main/java/com/zkzong/guava/CollectTest.java
+++ thirdparty/guava/src/main/java/com/zkzong/guava/CollectTest.java
@@ -18,49 +18,54 @@ import java.util.Set;
  * Created by Zong on 2016/10/11.
  */
 public class CollectTest {
+
     @Test
     public void set() {
-        Set allSet = new LinkedHashSet();
-        Map<String, Object> m1 = new HashMap<String, Object>();
+        Set<String> set = Sets.newHashSet("a", "b", "c");
+        System.out.println(set);
+
+        // difference union intersection
+        Set set1 = new LinkedHashSet();
+        Map<String, Object> m1 = new HashMap<>();
         m1.put("id", 1);
         m1.put("name", "zong");
-        allSet.add(m1);
-        Map<String, Object> m2 = new HashMap<String, Object>();
+        set1.add(m1);
+        Map<String, Object> m2 = new HashMap<>();
         m2.put("id", 2);
         m2.put("name", "wang");
-        allSet.add(m2);
-        Map<String, Object> m3 = new HashMap<String, Object>();
+        set1.add(m2);
+        Map<String, Object> m3 = new HashMap<>();
         m3.put("id", 3);
         m3.put("name", "liu");
-        allSet.add(m3);
-        Map<String, Object> m4 = new HashMap<String, Object>();
+        set1.add(m3);
+        Map<String, Object> m4 = new HashMap<>();
         m4.put("id", 4);
         m4.put("name", "ma");
-        allSet.add(m4);
-        Map<String, Object> m5 = new HashMap<String, Object>();
+        set1.add(m4);
+        Map<String, Object> m5 = new HashMap<>();
         m5.put("id", 5);
         m5.put("name", "zhang");
-        allSet.add(m5);
-        System.out.println("allSet: " + allSet);
+        set1.add(m5);
+        System.out.println("set1: " + set1);
 
-        Set set = new LinkedHashSet();
-        Map<String, Object> m7 = new HashMap<String, Object>();
-        m7.put("id", 5);
-        m7.put("name", "zhang");
-        set.add(m7);
-        Map<String, Object> m6 = new HashMap<String, Object>();
+        Set set2 = new LinkedHashSet();
+        Map<String, Object> m6 = new HashMap<>();
         m6.put("id", 4);
         m6.put("name", "ma");
-        set.add(m6);
-        Map<String, Object> m8 = new HashMap<String, Object>();
+        set2.add(m6);
+        Map<String, Object> m7 = new HashMap<>();
+        m7.put("id", 5);
+        m7.put("name", "zhang");
+        set2.add(m7);
+        Map<String, Object> m8 = new HashMap<>();
         m8.put("id", 8);
         m8.put("name", "ma");
-        set.add(m8);
-        System.out.println("set: " + set);
+        set2.add(m8);
+        System.out.println("set2: " + set2);
 
-        System.out.println("Sets.difference(allSet, set): " + Sets.difference(allSet, set));
-        System.out.println("Sets.union(allSet, set): " + Sets.union(allSet, set));
-        System.out.println("Sets.intersection(allSet, set): " + Sets.intersection(allSet, set));
+        System.out.println("Sets.difference(set1, set): " + Sets.difference(set1, set2));
+        System.out.println("Sets.union(set1, set): " + Sets.union(set1, set2));
+        System.out.println("Sets.intersection(set1, set): " + Sets.intersection(set1, set2));
     }
 
     @Test
@@ -75,6 +80,9 @@ public class CollectTest {
 
     @Test
     public void map() {
+        Map<String, String> map = Maps.newHashMap();
+        map.put("a", "a");
+        System.out.println(map);
     }
 
     @Test
