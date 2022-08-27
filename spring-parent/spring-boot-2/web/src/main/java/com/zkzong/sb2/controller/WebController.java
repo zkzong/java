1db508f1cc2d7ca38729500ef80bc01ab9607c62
zkzong <zongzhankui@hotmail.com>
2022-08-27 22:14:18 +0800

    Spring Boot

--- spring-parent/spring-boot-2/web/src/main/java/com/zkzong/sb2/controller/WebController.java
+++ spring-parent/spring-boot-2/web/src/main/java/com/zkzong/sb2/controller/WebController.java
@@ -7,7 +7,11 @@ import com.zkzong.sb2.dto.JsonRootBean;
 import com.zkzong.sb2.entity.User;
 import com.zkzong.sb2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
