1020d31b348a3a5b76244084fabe6134284e3d36
zkzong <zongzhankui@hotmail.com>
2022-09-18 12:44:51 +0800

    skywalking

--- skywalking/skywalking-itcast/skywalking_dubbo_consumer/src/main/java/com/itcast/api/IHelloService.java
+++
@@ -1,5 +0,0 @@
-package com.itcast.api;
-
-public interface IHelloService {
-    public String hello();
-}
--- skywalking/skywalking-itcast/skywalking_mysql/src/main/java/com/itcast/skywalking_mysql/dao/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itcast.skywalking_mysql.dao;
-
-import com.itcast.skywalking_mysql.pojo.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
