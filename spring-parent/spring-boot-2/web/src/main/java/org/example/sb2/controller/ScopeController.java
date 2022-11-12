ee1ba2af55577e4c67b85f5b2a59b81b56ce45d5
zongzhankui <zongzhankui@hotmail.com>
2022-11-12 14:05:51 +0800

    Controller单例

---
+++ spring-parent/spring-boot-2/web/src/main/java/org/example/sb2/controller/ScopeController.java
@@ -0,0 +1,31 @@
+package org.example.sb2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller是单例
+ */
+@RestController
+//@Scope("prototype")
+public class ScopeController {
+    private int num = 0;
+
+    ThreadLocal<Integer> uniqueNum = ThreadLocal.withInitial(() -> num);
+
+    @RequestMapping("/scope1")
+    public void testScope() {
+        int unum = uniqueNum.get();
+        uniqueNum.set(++unum);
+        System.out.println(uniqueNum.get());
+        //System.out.println(++num);
+    }
+
+    @RequestMapping("/scope2")
+    public void testScope2() {
+        int unum = uniqueNum.get();
+        uniqueNum.set(++unum);
+        System.out.println(uniqueNum.get());
+        //System.out.println(++num);
+    }
+}
