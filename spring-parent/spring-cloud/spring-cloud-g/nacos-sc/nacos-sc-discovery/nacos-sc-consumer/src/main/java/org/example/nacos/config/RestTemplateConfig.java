c3da6795b064eb347de6fecd54996eea0a92ddef
zkzong <zongzhankui@hotmail.com>
2022-11-23 23:46:18 +0800

    nacos

--- spring-parent/spring-cloud/spring-cloud-g/nacos-sc/nacos-sc-discovery/nacos-sc-consumer/src/main/java/org/example/nacos/config/RestTemplateConfig.java
+++ spring-parent/spring-cloud/spring-cloud-g/nacos-sc/nacos-sc-discovery/nacos-sc-consumer/src/main/java/org/example/nacos/config/RestTemplateConfig.java
@@ -1,35 +1,46 @@
 package org.example.nacos.config;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
-    // 1.
+    // 1. 已废弃
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
                 .setConnectTimeout(3000)
-           .setReadTimeout(3000)
-           .build();
+                .setReadTimeout(3000)
+                .build();
+    }
+
+    // 2. 使用ClientHttpRequestFactory配置RestTemplate
+    @Bean
+    public RestTemplate customRestTemplate1() {
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        httpRequestFactory.setConnectionRequestTimeout(3000);
+        httpRequestFactory.setConnectTimeout(3000);
+        httpRequestFactory.setReadTimeout(3000);
+
+        return new RestTemplate(httpRequestFactory);
+    }
+
+    // 3. 使用配置文件ClientHttpRequestFactory配置RestTemplate
+    @Bean
+    @ConfigurationProperties(prefix = "custom.rest.connection")
+    public HttpComponentsClientHttpRequestFactory customHttpRequestFactory() {
+        return new HttpComponentsClientHttpRequestFactory();
     }
 
     @Bean
-    public RestTemplate restTemplate(@Qualifier("simpleClientHttpRequestFactory") SimpleClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
+    public RestTemplate customRestTemplate2() {
+        return new RestTemplate(customHttpRequestFactory());
     }
 
-    @Bean
-    public SimpleClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(2000); // 毫秒
-        factory.setReadTimeout(10000); // 毫秒
-        return factory;
-    }
 }
 
