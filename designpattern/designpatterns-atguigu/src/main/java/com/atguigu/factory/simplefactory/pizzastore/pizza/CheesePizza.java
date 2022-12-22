6459183ca061f175bfa0b403f1c02712254bd139
zongzhankui <zongzhankui@hotmail.com>
2022-12-22 15:13:26 +0800

    设计模式

---
+++ designpattern/designpatterns-atguigu/src/main/java/com/atguigu/factory/simplefactory/pizzastore/pizza/CheesePizza.java
@@ -0,0 +1,10 @@
+package com.atguigu.factory.simplefactory.pizzastore.pizza;
+
+public class CheesePizza extends Pizza {
+
+    @Override
+    public void prepare() {
+        System.out.println(" 给制作奶酪披萨 准备原材料 ");
+    }
+
+}
