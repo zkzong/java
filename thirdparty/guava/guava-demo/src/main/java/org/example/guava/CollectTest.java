fc405d014c9d5d721cd650c5a4a0f7fad468ba7a
zongzhankui <zongzhankui@hotmail.com>
2022-12-26 17:43:28 +0800

    guava

--- thirdparty/guava/guava-demo/src/main/java/org/example/guava/CollectTest.java
+++ thirdparty/guava/guava-demo/src/main/java/org/example/guava/CollectTest.java
@@ -1,24 +1,39 @@
 package org.example.guava;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.MutableClassToInstanceMap;
 import com.google.common.collect.Range;
+import com.google.common.collect.RangeMap;
 import com.google.common.collect.Sets;
+import com.google.common.collect.TreeRangeMap;
 import com.google.common.math.LongMath;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.Test;
 
 import java.math.RoundingMode;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * Created by Zong on 2016/10/11.
  */
 public class CollectTest {
 
+    // ==============================Set==============================
     @Test
     public void set() {
         Set<String> set = Sets.newHashSet("a", "b", "c");
@@ -69,6 +84,21 @@ public class CollectTest {
     }
 
     @Test
+    public void multiset() {
+        Multiset<String> multiset = HashMultiset.create();
+
+        multiset.add("a");
+        multiset.add("a");
+        multiset.add("b");
+        multiset.add("c");
+        multiset.add("b");
+
+        System.out.println(multiset.size()); // 5
+        System.out.println(multiset.count("a")); // 2
+    }
+
+    // ==============================List==============================
+    @Test
     public void list() {
         List list = Lists.newArrayList();
         System.out.println(list.size());
@@ -78,11 +108,151 @@ public class CollectTest {
         System.out.println(list.size());
     }
 
+    // ==============================Map==============================
     @Test
     public void map() {
         Map<String, String> map = Maps.newHashMap();
         map.put("a", "a");
         System.out.println(map);
+    }
+
+    // BiMap - 双向Map
+    @Test
+    public void biMap() {
+        HashBiMap<String, String> biMap = HashBiMap.create();
+        biMap.put("Hydra", "Programmer");
+        biMap.put("Tony", "IronMan");
+        biMap.put("Thanos", "Titan");
+        //使用key获取value
+        System.out.println(biMap.get("Tony"));
+
+        BiMap<String, String> inverse = biMap.inverse();
+        //使用value获取key
+        System.out.println(inverse.get("Titan"));
+
+        // 反转后操作的影响
+        HashBiMap<String, String> biMap1 = HashBiMap.create();
+        biMap1.put("Hydra", "Programmer");
+        biMap1.put("Tony", "IronMan");
+        biMap1.put("Thanos", "Titan");
+        BiMap<String, String> inverse1 = biMap1.inverse();
+        System.out.println(inverse1);
+
+        inverse1.put("IronMan", "Stark");
+        System.out.println(biMap1);
+
+        // value不可重复
+        HashBiMap<String, String> biMap2 = HashBiMap.create();
+        biMap2.put("Tony", "IronMan");
+        biMap2.put("Stark", "IronMan");
+        //biMap.forcePut("Stark", "IronMan");
+        System.out.println(biMap2);
+    }
+
+    @Test
+    public void multiMap() {
+        Multimap<String, Integer> multimap = ArrayListMultimap.create();
+        multimap.put("day", 1);
+        multimap.put("day", 2);
+        multimap.put("day", 8);
+        multimap.put("month", 3);
+        System.out.println(multimap);
+
+        // 获取值的集合
+        Collection<Integer> day = multimap.get("day");
+        System.out.println(day);
+
+        ArrayListMultimap<String, Integer> multimap1 = ArrayListMultimap.create();
+        List<Integer> day1 = multimap1.get("day");
+        System.out.println(day1);
+
+        // 操作get后的集合
+        ArrayListMultimap<String, Integer> multimap2 = ArrayListMultimap.create();
+        multimap2.put("day", 1);
+        multimap2.put("day", 2);
+        multimap2.put("day", 8);
+        multimap2.put("month", 3);
+
+        List<Integer> day2 = multimap2.get("day");
+        List<Integer> month = multimap2.get("month");
+
+        day2.remove(0);//这个0是下标
+        month.add(12);
+        System.out.println(multimap2);
+
+        // 转换为Map
+        Map<String, Collection<Integer>> map = multimap.asMap();
+        for (String key : map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
+        map.get("day").add(20);
+        System.out.println(multimap);
+
+        // 数量问题
+        System.out.println(multimap.size());
+        System.out.println(multimap.entries().size());
+        for (Map.Entry<String, Integer> entry : multimap.entries()) {
+            System.out.println(entry.getKey() + "," + entry.getValue());
+        }
+        System.out.println(multimap.keySet().size());
+        Set<Map.Entry<String, Collection<Integer>>> entries = multimap.asMap().entrySet();
+        System.out.println(entries.size());
+
+    }
+
+    @Test
+    public void rangeMap() {
+        RangeMap<Integer, String> rangeMap = TreeRangeMap.create();
+        rangeMap.put(Range.closedOpen(0, 60), "fail");
+        rangeMap.put(Range.closed(60, 90), "satisfactory");
+        rangeMap.put(Range.openClosed(90, 100), "excellent");
+
+        System.out.println(rangeMap.get(59));
+        System.out.println(rangeMap.get(60));
+        System.out.println(rangeMap.get(90));
+        System.out.println(rangeMap.get(91));
+
+        rangeMap.remove(Range.closed(70, 80));
+        System.out.println(rangeMap.get(75));
+
+    }
+
+    @Test
+    public void classToInstanceMap() {
+        ClassToInstanceMap<Object> instanceMap = MutableClassToInstanceMap.create();
+        User user = new User("Hydra", 18);
+        Dept dept = new Dept("develop", 200);
+
+        instanceMap.putInstance(User.class, user);
+        instanceMap.putInstance(Dept.class, dept);
+
+        User user1 = instanceMap.getInstance(User.class);
+        System.out.println(user == user1);
+
+        ClassToInstanceMap<Map> instanceMap1 = MutableClassToInstanceMap.create();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        TreeMap<String, Object> treeMap = new TreeMap<>();
+
+        instanceMap1.putInstance(HashMap.class, hashMap);
+        instanceMap1.putInstance(TreeMap.class, treeMap);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    class User {
+
+        private String name;
+        private Integer age;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    class Dept {
+
+        private String name;
+        private Integer code;
     }
 
     @Test
--- thirdparty/guava/guava-demo/src/main/java/org/example/guava/TableTest.java
+++ thirdparty/guava/guava-demo/src/main/java/org/example/guava/TableTest.java
@@ -2,7 +2,12 @@ package org.example.guava;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
+import com.google.common.collect.Tables;
 import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by zong on 2016/8/10.
@@ -23,6 +28,41 @@ public class TableTest {
         //取出元素
         Integer dayCount = table.get("Hydra", "Feb");
         System.out.println(dayCount);
+
+        // 获得key或value的集合
+        //rowKey或columnKey的集合
+        Set<String> rowKeys = table.rowKeySet();
+        System.out.println("rowKeySet = " + rowKeys);
+        Set<String> columnKeys = table.columnKeySet();
+        System.out.println("columnKeySet = " + columnKeys);
+
+        //value集合
+        Collection<Integer> values = table.values();
+        System.out.println("values = " + values);
+
+        // 计算key对应的所有value的和
+        for (String key : table.rowKeySet()) {
+            Set<Map.Entry<String, Integer>> rows = table.row(key).entrySet();
+            int total = 0;
+            for (Map.Entry<String, Integer> row : rows) {
+                total += row.getValue();
+            }
+            System.out.println(key + ": " + total);
+        }
+
+        // 转换rowKey和columnKey
+        Table<String, String, Integer> table2 = Tables.transpose(table);
+        Set<Table.Cell<String, String, Integer>> cells = table2.cellSet();
+        cells.forEach(cell ->
+                System.out.println(cell.getRowKey() + "," + cell.getColumnKey() + ":" + cell.getValue())
+        );
+
+        // 转为嵌套的Map
+        Map<String, Map<String, Integer>> rowMap = table.rowMap();
+        System.out.println(rowMap);
+        Map<String, Map<String, Integer>> columnMap = table.columnMap();
+        System.out.println(columnMap);
+
 
     }
 }
