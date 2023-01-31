da742f8ad49b159a58079c3f9c0ac87e13636277
zkzong <zongzhankui@hotmail.com>
2023-01-31 22:21:37 +0800

    minio

--- spring-parent/spring-boot-2/minio/src/test/java/org/example/minio/service/MinioServiceTest.java
+++ spring-parent/spring-boot-2/minio/src/test/java/org/example/minio/service/MinioServiceTest.java
@@ -1,12 +1,14 @@
 package org.example.minio.service;
 
 import cn.hutool.core.io.FileUtil;
+import io.minio.ObjectWriteResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -14,6 +16,9 @@ import java.io.InputStream;
 public class MinioServiceTest {
 
     private String bucketName = "minio-example-demo";
+
+    @Autowired
+    private HttpServletResponse httpServletResponse;
 
     @Autowired
     private MinioService minioService;
@@ -38,25 +43,34 @@ public class MinioServiceTest {
 
     @Test
     public void putObject() {
+        ObjectWriteResponse objectWriteResponse = minioService.putObject(bucketName, "a.jpg", "D:/简读中国史.jpg");
+        System.out.println(objectWriteResponse);
     }
 
     @Test
     public void testPutObject() {
+        ObjectWriteResponse objectWriteResponse = minioService.putObject(bucketName, "aa.jpg", FileUtil.getInputStream("简读中国史.jpg"));
+        System.out.println(objectWriteResponse);
     }
 
     @Test
     public void uploadFileSingle() {
+        //minioService.uploadFileSingle(bucketName, new CommonsMultipartFile(new DiskFileItem()));
     }
 
     @Test
     public void uploadFileBatch() {
+        //minioService.uploadFileBatch(bucketName, )
     }
 
     @Test
     public void getUploadObjectUrl() {
+        String uploadObjectUrl = minioService.getUploadObjectUrl(bucketName, "简读中国史.jpg", 7);
+        System.out.println(uploadObjectUrl);
     }
 
     @Test
     public void download() {
+        minioService.download(bucketName, "简读中国史.jpg", httpServletResponse);
     }
 }
