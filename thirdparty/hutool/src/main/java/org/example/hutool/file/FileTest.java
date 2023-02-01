c342abc06bfde5650ed1d91741c6976a199137fd
zongzhankui <zongzhankui@hotmail.com>
2023-02-01 10:39:33 +0800

    hutool

--- thirdparty/hutool/src/main/java/org/example/hutool/file/FileTest.java
+++ thirdparty/hutool/src/main/java/org/example/hutool/file/FileTest.java
@@ -1,21 +1,46 @@
 package org.example.hutool.file;
 
 import cn.hutool.core.io.FileTypeUtil;
+import cn.hutool.core.io.FileUtil;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileTest {
 
+    /**
+     * 获取文件类型
+     */
     @Test
     public void fileType() {
-        File file = new File("D:/aaa.pdf");
+        String path = this.getClass().getClassLoader().getResource("").getPath();
+        File file = new File(path + "/aaa.pdf");
         String type = FileTypeUtil.getType(file);
         System.out.println(type);
 
-        InputStream inputStream = FileTest.class.getClassLoader().getResourceAsStream("aaa.pdf");
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("aaa.pdf");
         String type1 = FileTypeUtil.getType(inputStream);
         System.out.println(type1);
+    }
+
+    @Test
+    public void fileutil() {
+        // InputStream转File
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("aaa.pdf");
+        File file = FileUtil.writeFromStream(inputStream, "a-a.pdf");
+        System.out.println(file.getAbsolutePath());
+
+        // 文件追加内容
+        FileUtil.appendString("aa", "a.txt", StandardCharsets.UTF_8);
+
+        List<String> list = new ArrayList<>();
+        list.add("b");
+        list.add("bb");
+        list.add("bbb");
+        FileUtil.appendLines(list, "a.txt", StandardCharsets.UTF_8);
     }
 }
