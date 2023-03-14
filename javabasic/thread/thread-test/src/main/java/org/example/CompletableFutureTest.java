492347655c31d01c9f1610b979ffbd8f974e4914
zkzong <zongzhankui@hotmail.com>
2023-03-15 00:03:46 +0800

    thread

--- javabasic/thread/thread-test/src/main/java/org/example/CompletableFutureTest.java
+++ javabasic/thread/thread-test/src/main/java/org/example/CompletableFutureTest.java
@@ -4,8 +4,19 @@ import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class CompletableFutureTest {
+
+    @Test
+    public void threadstate() throws InterruptedException {
+        Thread thread = new Thread();
+        System.out.println("1- " + thread.getState());
+        thread.start();
+        System.out.println("2- " + thread.getState());
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("3- " + thread.getState());
+    }
 
     @Test
     public void a() throws ExecutionException, InterruptedException {
--- javabasic/thread/thread-test/src/main/java/org/example/thread/three/CallableTest.java
+++ javabasic/thread/thread-test/src/main/java/org/example/thread/three/CallableTest.java
@@ -3,6 +3,8 @@ package org.example.thread.three;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Zong on 2016/12/5.
@@ -10,16 +12,18 @@ import java.util.concurrent.FutureTask;
 public class CallableTest implements Callable {
     @Override
     public Object call() throws Exception {
+        TimeUnit.SECONDS.sleep(10);
         return "实现Callable接口";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TimeoutException {
         CallableTest ct = new CallableTest();
         FutureTask<String> task = new FutureTask<String>(ct);
         Thread t = new Thread(task);
         t.start();
         try {
-            System.out.println(task.get());
+            //System.out.println(task.get());
+            System.out.println(task.get(5, TimeUnit.SECONDS));
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
