22d1362055d164ced29cefdd6a658a72238040b3
zkzong <zongzhankui@hotmail.com>
2024-04-14 13:08:51 +0800

    int

---
+++ javabasic/interview/src/main/java/com/example/interview/IntTest.java
@@ -0,0 +1,13 @@
+package com.example.interview;
+
+import org.junit.Test;
+
+public class IntTest {
+
+    @Test
+    public void test() {
+        Integer i = new Integer(500);
+        int j = 500;
+        System.out.print(i == j); // true
+    }
+}
