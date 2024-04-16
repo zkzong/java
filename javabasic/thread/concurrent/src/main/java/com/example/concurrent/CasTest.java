aa64602c3e8095c67fa6ecc7217bd5a026fc07d9
zkzong <zongzhankui@hotmail.com>
2024-04-16 18:12:53 +0800

    cas

---
+++ javabasic/thread/concurrent/src/main/java/com/example/concurrent/CasTest.java
@@ -0,0 +1,38 @@
+//package com.example.concurrent;
+//
+///**
+// * @Author: zongz
+// * @Date: 2024/4/16
+// */
+//public class CasTest {
+//    private volatile int a;
+//
+//    public static void main(String[] args) {
+//        CasTest casTest = new CasTest();
+//        new Thread(() -> {
+//            for (int i = 1; i < 5; i++) {
+//                casTest.increment(i);
+//                System.out.print(casTest.a + " ");
+//            }
+//        }).start();
+//        new Thread(() -> {
+//            for (int i = 5; i < 10; i++) {
+//                casTest.increment(i);
+//                System.out.print(casTest.a + " ");
+//            }
+//        }).start();
+//    }
+//
+//    private void increment(int x) {
+//        while (true) {
+//            try {
+//                long fieldOffset = unsafe.objectFieldOffset(CasTest.class.getDeclaredField("a"));
+//                if (unsafe.compareAndSwapInt(this, fieldOffset, x - 1, x))
+//                    break;
+//            } catch (NoSuchFieldException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//}
