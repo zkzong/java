e156420ca0d821f89829338dfa19471f84b25605
zkzong <zongzhankui@hotmail.com>
2024-05-16 22:39:25 +0800

    设计模式

--- designpattern/designpatterns-atguigu/src/main/java/com/atguigu/singleton/type8/SingletonTest08.java
+++ designpattern/designpatterns-atguigu/src/main/java/com/atguigu/singleton/type8/SingletonTest08.java
@@ -4,7 +4,7 @@ public class SingletonTest08 {
     public static void main(String[] args) {
         Singleton instance = Singleton.INSTANCE;
         Singleton instance2 = Singleton.INSTANCE;
-        System.out.println(instance == instance2);
+        System.out.println(instance == instance2); // true
 
         System.out.println(instance.hashCode());
         System.out.println(instance2.hashCode());
