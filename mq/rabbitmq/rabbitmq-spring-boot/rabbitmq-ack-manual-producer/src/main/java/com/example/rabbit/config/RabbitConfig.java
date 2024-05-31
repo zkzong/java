b8d37be2493d931bd07c09058ba56f0de0f209ae
zkzong <zongzhankui@hotmail.com>
2024-05-31 20:41:20 +0800

    rabbit

--- mq/rabbitmq/rabbitmq-spring-boot/rabbitmq-ack-manual-producer/src/main/java/com/example/rabbit/config/RabbitConfig.java
+++ mq/rabbitmq/rabbitmq-spring-boot/rabbitmq-ack-manual-producer/src/main/java/com/example/rabbit/config/RabbitConfig.java
@@ -1,9 +1,66 @@
 package com.example.rabbit.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
 public class RabbitConfig {
 
     public static final String QUEUE_NAME = "test.queue";
     public static final String EXCHANGE_NAME = "test.exchange";
     public static final String ROUTING_KEY = "test.routing.key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+
+        rabbitTemplate.setMandatory(true);
+
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                if (ack) {
+                    log.info("correlationData={}, ack={}, cause={}", correlationData, ack, cause);
+                } else {
+                    log.info("correlationData={}, ack={}, cause={}", correlationData, ack, cause);
+                }
+            }
+        });
+
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                log.info("message={}, replyCode={}, replyText={}, exchange={}, routingKey={}", message, replyCode, replyText, exchange, routingKey);
+            }
+        });
+
+        return rabbitTemplate;
+    }
 
 }
--- mq/rabbitmq/rabbitmq-spring-boot/rabbitmq-ack-manual-producer/src/main/java/com/example/rabbit/controller/ProducerController.java
+++ mq/rabbitmq/rabbitmq-spring-boot/rabbitmq-ack-manual-producer/src/main/java/com/example/rabbit/controller/ProducerController.java
@@ -1,6 +1,7 @@
 package com.example.rabbit.controller;
 
-import com.example.rabbit.Producer;
+import com.example.rabbit.config.RabbitConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProducerController {
 
     @Autowired
-    private Producer producer;
+    private RabbitTemplate rabbitTemplate;
 
     @RequestMapping("/send")
     public String send() {
-        producer.send();
+
+        // 在RabbitCongig中绑定了exchange-test和routingkey-test，所以这里需要指定exchange和routingkey
+        rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_NAME, RabbitConfig.ROUTING_KEY, "hello rabbitmq");
+
+        rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_NAME + 1, RabbitConfig.ROUTING_KEY, "hello rabbitmq");
+
+        rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_NAME, RabbitConfig.ROUTING_KEY + 1, "hello rabbitmq");
+
         return "send";
     }
 }
