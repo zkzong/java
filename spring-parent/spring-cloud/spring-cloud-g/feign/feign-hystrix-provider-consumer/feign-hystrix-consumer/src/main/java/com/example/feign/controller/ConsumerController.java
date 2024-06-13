38240bac2b80fbfa533d2c14a09dc3a152d72fc2
zkzong <zongzhankui@hotmail.com>
2024-06-13 15:11:15 +0800

    hystrix

--- spring-parent/spring-cloud/spring-cloud-g/feign/feign-hystrix-provider-consumer/feign-hystrix-consumer/src/main/java/com/example/feign/controller/ConsumerController.java
+++ spring-parent/spring-cloud/spring-cloud-g/feign/feign-hystrix-provider-consumer/feign-hystrix-consumer/src/main/java/com/example/feign/controller/ConsumerController.java
@@ -3,6 +3,7 @@ package com.example.feign.controller;
 import com.example.feign.api.ProviderClient;
 import com.example.feign.api.ProviderClient2;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +34,41 @@ public class ConsumerController {
         return "fallbackMethod";
     }
 
+    /**
+     * Hystrix断路器设置
+     *
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"),//是否开启断路器
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10"),// 请求次数
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10000"),// 时间窗口期
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")// 失败率达到多少后跳
+    })
+    @RequestMapping("/circuitbreaker")
+    public String circuitbreaker() {
+        int i = 1 / 0;
+        return "ratelimit";
+    }
+
+    /**
+     * Hystrix限流
+     *
+     * @return
+     */
+    @HystrixCommand(
+            groupKey = "threadA",
+            threadPoolKey = "threadA",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "20"),
+                    @HystrixProperty(name = "maximumSize", value = "30"),
+                    @HystrixProperty(name = "maxQueueSize", value = "20"),
+                    @HystrixProperty(name = "keepAliveTimeMinutes", value = "2")
+            }, commandProperties = {
+            @HystrixProperty(name = "execution.isolation.strategy", value = "THREAD")
+    })
+    @RequestMapping("/ratelimit")
+    public String ratelimit() {
+        return "ratelimit";
+    }
 }
