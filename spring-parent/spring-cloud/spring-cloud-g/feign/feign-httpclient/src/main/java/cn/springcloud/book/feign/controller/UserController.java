6b0db6f07ebccc04336ff6957467571bcc317f63
zkzong <zongzhankui@hotmail.com>
2024-09-01 08:39:40 +0800

    feign

---
+++ spring-parent/spring-cloud/spring-cloud-g/feign/feign-httpclient/src/main/java/cn/springcloud/book/feign/controller/UserController.java
@@ -0,0 +1,22 @@
+package cn.springcloud.book.feign.controller;
+
+import cn.springcloud.book.feign.entity.UserRequest;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zongz
+ * @Date: 2024/9/1
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/get")
+    public String getUser(@SpringQueryMap UserRequest userRequest) {
+        return userRequest.getUsername() + " " + userRequest.getPassword();
+    }
+
+}
---
+++ spring-parent/spring-cloud/spring-cloud-g/feign/feign-httpclient/src/main/java/cn/springcloud/book/feign/entity/UserRequest.java
@@ -0,0 +1,15 @@
+package cn.springcloud.book.feign.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: zongz
+ * @Date: 2024/9/1
+ */
+@Data
+public class UserRequest {
+
+    private String username;
+    private String password;
+
+}
