98b513d04b9e243b0a3c08540ae05be5ca11ffab
zkzong <zongzhankui@hotmail.com>
2024-10-21 23:34:04 +0800

    oss 上传和下载

--- thirdparty/oss/src/test/java/org/example/oss/OssApplicationTest.java
+++ thirdparty/oss/src/test/java/org/example/oss/OssApplicationTest.java
@@ -1,15 +1,19 @@
 package org.example.oss;
 
 import com.aliyun.oss.OSS;
+import com.aliyun.oss.model.*;
 import org.example.oss.config.OssProperties;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
+import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -28,25 +32,60 @@ public class OssApplicationTest {
 
     private String key;
 
+    private File file;
+
     @Before
     public void init() {
         key = "test/baidu.png";
+
+        String path = this.getClass().getClassLoader().getResource("").getPath();
+        file = new File(path + "/baidu.png");
     }
 
     @Test
     public void putObject() {
-        String path = this.getClass().getClassLoader().getResource("").getPath();
-        File file = new File(path + "/baidu.png");
         ossClient.putObject(ossProperties.getBucketName(), key, file);
     }
 
     @Test
-    public void generatePresignedUrl() {
+    public void uploadFile() throws Throwable {
+        UploadFileRequest uploadFileRequest = new UploadFileRequest(ossProperties.getBucketName(), key);
+        uploadFileRequest.setUploadFile(file.getAbsolutePath());
+        UploadFileResult uploadFileResult = ossClient.uploadFile(uploadFileRequest);
+    }
+
+    @Test
+    public void downloadFile() throws Throwable {
+        DownloadFileRequest downloadFileRequest = new DownloadFileRequest(ossProperties.getBucketName(), key);
+        downloadFileRequest.setDownloadFile("a.txt");
+        DownloadFileResult downloadFileResult = ossClient.downloadFile(downloadFileRequest);
+    }
+
+    private Date date;
+
+    @Before
+    public void setDate() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
         calendar.add(Calendar.DAY_OF_YEAR, 30);
-        Date date = calendar.getTime();
-        ossClient.generatePresignedUrl(ossProperties.getBucketName(), key, date);
+        date = calendar.getTime();
+    }
+
+    @Test
+    public void generatePresignedUrl() {
+        URL url = ossClient.generatePresignedUrl(ossProperties.getBucketName(), key, date);
+    }
+
+    @Test
+    public void aVoid() {
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(ossProperties.getBucketName(), key);
+        generatePresignedUrlRequest.setExpiration(date);
+        ResponseHeaderOverrides responseHeaderOverrides = new ResponseHeaderOverrides();
+        String contentType = MediaTypeFactory.getMediaType(key).orElse(MediaType.APPLICATION_OCTET_STREAM).toString();
+        // 当需要在浏览器里直接下载时，设置content-type为application/octet-stream，浏览器会直接下载文件，而不是打开文件
+        responseHeaderOverrides.setContentType(contentType.toUpperCase());
+        generatePresignedUrlRequest.setResponseHeaders(responseHeaderOverrides);
+        URL url = ossClient.generatePresignedUrl(generatePresignedUrlRequest);
     }
 
 }
