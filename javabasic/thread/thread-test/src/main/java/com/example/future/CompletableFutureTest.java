2a0ff84fb685d1ec738e8a83bcd1cda4d9a96550
zkzong <zongzhankui@hotmail.com>
2024-12-21 00:51:57 +0800

    CompletableFuture

--- javabasic/thread/thread-test/src/main/java/com/example/future/CompletableFutureTest.java
+++ javabasic/thread/thread-test/src/main/java/com/example/future/CompletableFutureTest.java
@@ -28,4 +28,37 @@ public class CompletableFutureTest {
         //CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "三友");
         //System.out.println(completableFuture.get());
     }
+
+    @Test
+    public void anyOf() {
+        // 创建两个CompletableFuture任务
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000); // 模拟任务执行时间
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return "Task 1 completed";
+        });
+
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(1000); // 模拟任务执行时间
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return "Task 2 completed";
+        });
+
+        // 使用anyOf方法，当任意一个任务完成时，新的CompletableFuture也会完成
+        CompletableFuture<Object> anyOfFuture = CompletableFuture.anyOf(future1, future2);
+
+        try {
+            // 等待任意一个任务完成
+            Object result = anyOfFuture.get();
+            System.out.println("First completed task result: " + result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
