901f9d01bf71b48f3b5a8cf2a5446d7f5213aa5c
zkzong <zongzhankui@hotmail.com>
2025-01-25 14:56:26 +0800

    AES

--- thirdparty/hutool/src/test/java/com/example/hutool/crypto/AesTest.java
+++ thirdparty/hutool/src/test/java/com/example/hutool/crypto/AesTest.java
@@ -1,5 +1,7 @@
 package com.example.hutool.crypto;
 
+import cn.hutool.crypto.Mode;
+import cn.hutool.crypto.Padding;
 import cn.hutool.crypto.SecureUtil;
 import cn.hutool.crypto.symmetric.AES;
 import org.junit.Test;
@@ -16,6 +18,12 @@ public class AesTest {
         String encryptStr = aes.encryptHex("data");
         System.out.println(encryptStr);
         String decryptStr = aes.decryptStr(encryptStr);
+        System.out.println(decryptStr);
+
+        aes = new AES(Mode.CBC, Padding.PKCS5Padding, "keyskeyskeyskeys".getBytes(), "keyskeyskeyskeys".getBytes());
+        encryptStr = aes.encryptHex("data");
+        System.out.println(encryptStr);
+        decryptStr = aes.decryptStr(encryptStr);
         System.out.println(decryptStr);
     }
 }
