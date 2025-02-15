48ff42076483ce69ce6002dbcdc3b9a803e0f671
zkzong <zongzhankui@hotmail.com>
2025-02-15 17:45:49 +0800

    apollo

--- spring-parent/spring-boot-2/apollo/src/main/java/com/example/apollo/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.example.apollo.config;
-//
-//import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@EnableApolloConfig(value = "application", order = 10)
-//public class AppConfig {
-//}
