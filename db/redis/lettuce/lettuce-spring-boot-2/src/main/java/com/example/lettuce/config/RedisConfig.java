f261d7fb5ad4b59c463c9f8c4c7994cdbb166530
zkzong <zongzhankui@hotmail.com>
2025-11-09 16:13:23 +0800

    redis读写分离

--- db/redis/lettuce/lettuce-spring-boot-2/src/main/java/com/example/lettuce/config/RedisConfig.java
+++ db/redis/lettuce/lettuce-spring-boot-2/src/main/java/com/example/lettuce/config/RedisConfig.java
@@ -1,7 +1,10 @@
 package com.example.lettuce.config;
 
+import io.lettuce.core.ReadFrom;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -22,5 +25,16 @@ public class RedisConfig {
         GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
         template.setValueSerializer(genericJackson2JsonRedisSerializer);
         return template;
+    }
+
+    // 读写分离
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer customizer() {
+        return new LettuceClientConfigurationBuilderCustomizer() {
+            @Override
+            public void customize(LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigurationBuilder) {
+                clientConfigurationBuilder.readFrom(ReadFrom.SLAVE_PREFERRED);
+            }
+        };
     }
 }
